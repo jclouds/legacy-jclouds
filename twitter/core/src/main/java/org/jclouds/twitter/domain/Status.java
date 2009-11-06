5d4a5622fcb60509e918ccac56c20301f0db48be
adrian.f.cole <adrian.f.cole@3d8758e0-26b5-11de-8745-db77d3ebf521>
2009-11-06 21:07:35 +0000

    fix file headers
    git-svn-id: http://jclouds.googlecode.com/svn/trunk@2051 3d8758e0-26b5-11de-8745-db77d3ebf521

--- twitter/core/src/main/java/org/jclouds/twitter/domain/Status.java
+++ twitter/core/src/main/java/org/jclouds/twitter/domain/Status.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2009 Cloud Conscious, LLC. <info@cloudconscious.com;
+ * Copyright (C) 2009 Cloud Conscious, LLC. <info@cloudconscious.com>
  *
  * ====================================================================
  * Licensed to the Apache Software Foundation (ASF) under one
