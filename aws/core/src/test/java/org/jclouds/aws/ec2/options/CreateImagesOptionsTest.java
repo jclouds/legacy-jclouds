d60951fcf12c57df4172be27952b050ae5545698
adrian.f.cole <adrian.f.cole@3d8758e0-26b5-11de-8745-db77d3ebf521>
2009-12-26 18:02:18 +0000

    Issue 29: options for create image
    git-svn-id: http://jclouds.googlecode.com/svn/trunk@2523 3d8758e0-26b5-11de-8745-db77d3ebf521

---
+++ aws/core/src/test/java/org/jclouds/aws/ec2/options/CreateImagesOptionsTest.java
@@ -0,0 +1,87 @@
+/**
+ *
+ * Copyright (C) 2009 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ */
+package org.jclouds.aws.ec2.options;
+
+import static org.jclouds.aws.ec2.options.CreateImageOptions.Builder.noReboot;
+import static org.jclouds.aws.ec2.options.CreateImageOptions.Builder.withDescription;
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+
+import org.jclouds.http.options.HttpRequestOptions;
+import org.testng.annotations.Test;
+
+/**
+ * Tests possible uses of CreateImageOptions and CreateImageOptions.Builder.*
+ * 
+ * @author Adrian Cole
+ */
+public class CreateImagesOptionsTest {
+
+   @Test
+   public void testAssignability() {
+      assert HttpRequestOptions.class.isAssignableFrom(CreateImageOptions.class);
+      assert !String.class.isAssignableFrom(CreateImageOptions.class);
+   }
+
+   @Test
+   public void testWithDescription() {
+      CreateImageOptions options = new CreateImageOptions();
+      options.withDescription("test");
+      assertEquals(options.buildFormParameters().get("Description"), Collections
+               .singletonList("test"));
+   }
+
+   @Test
+   public void testNullWithDescription() {
+      CreateImageOptions options = new CreateImageOptions();
+      assertEquals(options.buildFormParameters().get("Description"), Collections.EMPTY_LIST);
+   }
+
+   @Test
+   public void testWithDescriptionStatic() {
+      CreateImageOptions options = withDescription("test");
+      assertEquals(options.buildFormParameters().get("Description"), Collections
+               .singletonList("test"));
+   }
+
+   @Test(expectedExceptions = NullPointerException.class)
+   public void testWithDescriptionNPE() {
+      withDescription(null);
+   }
+
+   @Test
+   public void testNoReboot() {
+      CreateImageOptions options = new CreateImageOptions();
+      options.noReboot();
+      assertEquals(options.buildFormParameters().get("NoReboot"), Collections.singletonList("true"));
+   }
+
+   @Test
+   public void testNoRebootStatic() {
+      CreateImageOptions options = noReboot();
+      assertEquals(options.buildFormParameters().get("NoReboot"), Collections.singletonList("true"));
+   }
+
+}
