9244b6ddd06d0b16678466a70e5548a9609eb40e
Adrian Cole <adrian@jclouds.org>
2010-02-09 00:23:51 -0800

    updated ant samples

--- tools/antcontrib/samples/cargooverssh/runscript.sh
+++ tools/antcontrib/samples/cargooverssh/runscript.sh
@@ -3,8 +3,8 @@ apt-get update -qq
 apt-get upgrade -y -qq
 apt-get install -y -qq wget
 apt-get install -y -qq openjdk-6-jdk
-wget -q http://www.alliedquotes.com/mirrors/apache/tomcat/tomcat-6/v6.0.20/bin/apache-tomcat-6.0.20.tar.gz
-tar xzf apache-tomcat-6.0.20.tar.gz
+wget -q http://mirrors.axint.net/apache/tomcat/tomcat-6/v6.0.24/bin/apache-tomcat-6.0.24.tar.gz
+tar xzf apache-tomcat-6.0.24.tar.gz
 mkdir -p /tmp/cargo/containers
 chmod 1777 /tmp/cargo
-mv apache-tomcat-6.0.20 /tmp/cargo/containers/tomcat6x
+mv apache-tomcat-6.0.24 /tmp/cargo/containers/tomcat6x
