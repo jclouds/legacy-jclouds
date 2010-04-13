b74a203401d86bb1aa94939a1445f5611766fd04
Chas Emerick <cemerick@snowtide.com>
2010-04-13 13:52:18 -0400

    add copyright notice and attribution to clojure ebs wrapper

--- aws/core/src/main/clojure/org/jclouds/aws/ebs.clj
+++ aws/core/src/main/clojure/org/jclouds/aws/ebs.clj
@@ -1,8 +1,27 @@
-(ns org.jclouds.aws.ebs
- "A clojure binding to the jclouds EBS service interface."
- (:require (org.jclouds [compute :as compute])
-   (clojure.contrib [logging :as log]))
- (:use (clojure.contrib def core))
+;;
+;; Copyright (C) 2010 Cloud Conscious, LLC. <info@cloudconscious.com>
+;;
+;; ====================================================================
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;; http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+;; ====================================================================
+;;
+
+(ns
+  #^{:author "Chas Emerick, cemerick@snowtide.com"
+     :doc "A clojure binding to the jclouds EBS service interface."}
+  org.jclouds.aws.ebs
+  (:require (org.jclouds [compute :as compute]))
+  (:use (clojure.contrib def core))
   (:import org.jclouds.aws.domain.Region
     org.jclouds.aws.ec2.domain.AvailabilityZone
     (org.jclouds.aws.ec2.options DescribeSnapshotsOptions DetachVolumeOptions CreateSnapshotOptions)))
