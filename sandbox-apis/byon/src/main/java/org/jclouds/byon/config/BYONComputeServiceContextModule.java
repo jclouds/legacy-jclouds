ceaddcc57f8522583667be62199400c896c75bdc
Kelvin Kakugawa <kakugawa@gmail.com>
2011-01-25 10:52:06 -0800

    read in byon config via yaml

--- sandbox-apis/byon/src/main/java/org/jclouds/byon/config/BYONComputeServiceContextModule.java
+++ sandbox-apis/byon/src/main/java/org/jclouds/byon/config/BYONComputeServiceContextModule.java
@@ -19,9 +19,14 @@
 
 package org.jclouds.byon.config;
 
+import java.io.InputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.inject.Singleton;
 
 import org.jclouds.byon.Node;
@@ -29,10 +34,17 @@ import org.jclouds.byon.internal.BYONComputeServiceAdapter;
 import org.jclouds.compute.config.JCloudsNativeComputeServiceAdapterContextModule;
 import org.jclouds.concurrent.SingleThreaded;
 import org.jclouds.location.Provider;
+import org.jclouds.logging.Logger;
 
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Provides;
+
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.Loader;
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.error.YAMLException;
 
 /**
  * 
@@ -42,6 +54,8 @@ import com.google.inject.Provides;
 @SingleThreaded
 public class BYONComputeServiceContextModule extends
       JCloudsNativeComputeServiceAdapterContextModule<Supplier, Supplier> {
+   @Resource
+   protected Logger logger = Logger.NULL;
 
    public BYONComputeServiceContextModule() {
       super(Supplier.class, Supplier.class, BYONComputeServiceAdapter.class);
@@ -49,15 +63,33 @@ public class BYONComputeServiceContextModule extends
 
    @Provides
    @Singleton
-   Supplier<Map<String, Node>> provideNodeList(@Provider URI uri) {
+   Supplier<Map<String, Node>> provideNodeList(@Provider final URI uri) {
       return new Supplier<Map<String, Node>> (){
-
          @Override
          public Map<String, Node> get() {
-            // TODO parse uri into list, using yaml or something
-            return ImmutableMap.<String, Node> of();
+            Constructor constructor = new Constructor(Config.class);
+
+            TypeDescription configDesc = new TypeDescription(Config.class);
+            configDesc.putMapPropertyType("nodes", String.class, Node.class);
+            constructor.addTypeDescription(configDesc);
+
+            Yaml yaml = new Yaml(new Loader(constructor));
+
+            Config config;
+            try {
+               URL url = uri.toURL();
+               InputStream input = url.openStream();
+               config = (Config)yaml.load(input);
+            } catch(MalformedURLException e) {
+               logger.error(e, "URI is not a URL: %s", uri);
+               return ImmutableMap.<String, Node> of();
+            } catch(IOException e) {
+               logger.error(e, "URI could not be read: %s", uri);
+               return ImmutableMap.<String, Node> of();
+            }
+
+            return config.nodes;
          }
-         
       };
    }
 
