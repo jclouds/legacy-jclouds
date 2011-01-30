672db20eb19677abd9a4a5bd29fae7c97c942f7e
Adrian Cole <adrian@jclouds.org>
2011-01-30 00:55:33 -0800

    walrus doesn't support range requests, content encoding or language

--- apis/walrus/src/test/java/org/jclouds/walrus/blobstore/WalrusBlobIntegrationLiveTest.java
+++ apis/walrus/src/test/java/org/jclouds/walrus/blobstore/WalrusBlobIntegrationLiveTest.java
@@ -19,14 +19,42 @@
 
 package org.jclouds.walrus.blobstore;
 
+import java.io.IOException;
+
+import org.jclouds.blobstore.domain.Blob;
 import org.jclouds.s3.blobstore.integration.S3BlobIntegrationLiveTest;
 import org.testng.annotations.Test;
 
 /**
  * @author Adrian Cole
  */
-@Test(groups =  "live", testName = "WalrusBlobIntegrationLiveTest")
+@Test(groups = "live", testName = "WalrusBlobIntegrationLiveTest")
 public class WalrusBlobIntegrationLiveTest extends S3BlobIntegrationLiveTest {
 
+   // no support for content encoding
+   @Override
+   protected void checkContentEncoding(Blob blob, String contentEncoding) {
+      assert blob.getPayload().getContentMetadata().getContentEncoding() == null;
+      assert blob.getMetadata().getContentMetadata().getContentEncoding() == null;
+   }
 
+   // no support for content language
+   @Override
+   protected void checkContentLanguage(Blob blob, String contentLanguage) {
+      assert blob.getPayload().getContentMetadata().getContentLanguage() == null;
+      assert blob.getMetadata().getContentMetadata().getContentLanguage() == null;
+   }
+
+   // double range not supported
+   @Test(groups = { "integration", "live" })
+   @Override
+   public void testGetTwoRanges() throws InterruptedException, IOException {
+
+   }
+
+   // range not supported
+   @Test(groups = { "integration", "live" })
+   @Override
+   public void testGetRange() throws InterruptedException, IOException {
+   }
 }
