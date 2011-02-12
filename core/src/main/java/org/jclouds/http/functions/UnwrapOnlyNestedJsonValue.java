096b25509d7b10835957c6d0a70980c86ba6d80c
Adrian Cole <adrian@jclouds.org>
2011-02-13 00:06:26 +0100

    updated nested parser so that it can return empty collections

--- core/src/main/java/org/jclouds/http/functions/UnwrapOnlyNestedJsonValue.java
+++ core/src/main/java/org/jclouds/http/functions/UnwrapOnlyNestedJsonValue.java
@@ -19,7 +19,9 @@
 
 package org.jclouds.http.functions;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -27,7 +29,11 @@ import javax.inject.Singleton;
 import org.jclouds.http.HttpResponse;
 
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
+import com.google.inject.TypeLiteral;
 
 /**
  * @author Adrian Cole
@@ -36,20 +42,30 @@ import com.google.common.collect.Iterables;
 public class UnwrapOnlyNestedJsonValue<T> implements Function<HttpResponse, T> {
 
    private final ParseJson<Map<String, Map<String, T>>> json;
+   private final TypeLiteral<T> type;
 
    @Inject
-   UnwrapOnlyNestedJsonValue(ParseJson<Map<String, Map<String, T>>> json) {
+   UnwrapOnlyNestedJsonValue(ParseJson<Map<String, Map<String, T>>> json, TypeLiteral<T> type) {
       this.json = json;
+      this.type = type;
    }
 
+   @SuppressWarnings("unchecked")
    @Override
    public T apply(HttpResponse arg0) {
       Map<String, Map<String, T>> map = json.apply(arg0);
       if (map == null || map.size() == 0)
          return null;
       Map<String, T> map1 = Iterables.getOnlyElement(map.values());
-      if (map1 == null || map1.size() == 0)
+      if (map1 == null || map1.size() == 0) {
+         if (type.getRawType().isAssignableFrom(Set.class))
+            return (T) ImmutableSet.of();
+         else if (type.getRawType().isAssignableFrom(List.class))
+            return (T) ImmutableList.of();
+         else if (type.getRawType().isAssignableFrom(Map.class))
+            return (T) ImmutableMap.of();
          return null;
+      }
       return Iterables.getOnlyElement(map1.values());
    }
 }
