4c4f89321125d73282ed6e79c7490cd5932c2464
Adrian Cole <adrian@jclouds.org>
2011-02-15 15:35:30 +0100

    Issue 474: updated example to use gae sdk 1.4.0

--- demos/googleappengine/src/main/java/org/jclouds/samples/googleappengine/functions/BlobStoreContextToStatusResult.java
+++ demos/googleappengine/src/main/java/org/jclouds/samples/googleappengine/functions/BlobStoreContextToStatusResult.java
@@ -19,79 +19,39 @@
 
 package org.jclouds.samples.googleappengine.functions;
 
-import java.util.Map;
-
 import javax.annotation.Resource;
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.jclouds.blobstore.BlobStoreContext;
-import org.jclouds.blobstore.ContainerNotFoundException;
-import org.jclouds.blobstore.domain.StorageMetadata;
-import org.jclouds.blobstore.domain.StorageType;
 import org.jclouds.logging.Logger;
 import org.jclouds.samples.googleappengine.domain.StatusResult;
 
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Sets;
 
+/**
+ * 
+ * @author Adrian Cole
+ */
 @Singleton
-public class BlobStoreContextToStatusResult implements Function<String, StatusResult> {
-   private final class BuildContainerResult implements Function<StorageMetadata, StatusResult> {
-      private final String host;
-      private final BlobStoreContext context;
-      private final String contextName;
-
-      private BuildContainerResult(String host, BlobStoreContext context, String contextName) {
-         this.host = host;
-         this.context = context;
-         this.contextName = contextName;
-      }
-
-      public StatusResult apply(StorageMetadata from) {
-         String status;
-         try {
-            try {
-               long start = System.currentTimeMillis();
-               context.getBlobStore().containerExists(from.getName());
-               status = ((System.currentTimeMillis() - start) + "ms");
-            } catch (ContainerNotFoundException ex) {
-               status = ("not found");
-            }
-         } catch (Exception e) {
-            logger.error(e, "Error listing container %s//%s", contextName, from);
-            status = (e.getMessage());
-         }
-         return new StatusResult(contextName, host, from.getName(), status);
-      }
-   }
-
-   @Inject
-   private Map<String, BlobStoreContext> contexts;
+public class BlobStoreContextToStatusResult implements Function<BlobStoreContext, StatusResult> {
 
    @Resource
    protected Logger logger = Logger.NULL;
 
-   public StatusResult apply(final String contextName) {
-      final BlobStoreContext context = contexts.get(contextName);
-      final String host = context.getProviderSpecificContext().getEndpoint().getHost();
+   public StatusResult apply(BlobStoreContext in) {
+      String host = in.getProviderSpecificContext().getEndpoint().getHost();
+      String status;
+      String name = "not found";
       try {
-         StorageMetadata md = Iterables.getLast(Sets.newTreeSet(Iterables.filter(context
-                  .getBlobStore().list(), new Predicate<StorageMetadata>() {
+         long start = System.currentTimeMillis();
 
-            public boolean apply(StorageMetadata input) {
-               return input.getType() == StorageType.CONTAINER;
-            }
+         name = String.format("%d containers", in.getBlobStore().list().size());
 
-         })));
-         return new BuildContainerResult(host, context, contextName).apply(md);
+         status = ((System.currentTimeMillis() - start) + "ms");
       } catch (Exception e) {
-         StatusResult result = new StatusResult(contextName, host, null, e.getMessage());
-         logger.error(e, "Error listing service %s", contextName);
-         return result;
+         logger.error(e, "Error listing context %s", in);
+         status = (e.getMessage());
       }
-
+      return new StatusResult(in.getProviderSpecificContext().getId(), host, name, status);
    }
 }
