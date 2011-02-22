5075032edd85b6eb3cd2e9284c46f8605caa46eb
Adrian Cole <adrian@jclouds.org>
2011-02-22 11:08:21 -0800

    added new param parser for comma-separated lists

---
+++ core/src/main/java/org/jclouds/functions/JoinOnComma.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * Copyright (C) 2011 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+
+package org.jclouds.functions;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.lang.reflect.Array;
+
+import javax.inject.Singleton;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.ImmutableList.Builder;
+
+/**
+ * 
+ * @author Adrian Cole
+ * 
+ */
+@Singleton
+public class JoinOnComma implements Function<Object, String> {
+
+   public String apply(Object o) {
+      checkNotNull(o, "input cannot be null");
+      if (o.getClass().isArray()) {
+         Builder<Object> builder = ImmutableList.builder();
+         for (int i = 0; i < Array.getLength(o); i++)
+            builder.add(Array.get(o, i));
+         o = builder.build();
+      }
+      checkArgument(o instanceof Iterable<?>, "you must pass an iterable or array");
+      Iterable<?> toJoin = (Iterable<?>) o;
+      checkArgument(Iterables.size(toJoin) > 0, "you must pass an iterable or array with elements");
+      return Joiner.on(',').join(toJoin);
+   }
+}
---
+++ core/src/test/java/org/jclouds/functions/JoinOnCommaTest.java
@@ -0,0 +1,61 @@
+/**
+ *
+ * Copyright (C) 2010 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+
+package org.jclouds.functions;
+
+import static org.testng.Assert.assertEquals;
+
+import org.jclouds.functions.JoinOnComma;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * 
+ * @author Adrian Cole
+ */
+@Test(groups = "unit", sequential = true)
+public class JoinOnCommaTest {
+   @Test
+   public void testIterableLong() {
+      String list = new JoinOnComma().apply(ImmutableList.of(1l, 2l));
+      assertEquals(list, "1,2");
+   }
+
+   @Test
+   public void testLongArray() {
+      String list = new JoinOnComma().apply(new long[] { 1l, 2l });
+      assertEquals(list, "1,2");
+   }
+
+   @Test(expectedExceptions = IllegalArgumentException.class)
+   public void testEmptyArrayIllegalArgumentException() {
+      new JoinOnComma().apply(new long[] {});
+   }
+
+   @Test(expectedExceptions = IllegalArgumentException.class)
+   public void testEmptyIterableIllegalArgumentException() {
+      new JoinOnComma().apply(ImmutableList.of());
+   }
+
+   @Test(expectedExceptions = NullPointerException.class)
+   public void testNullPointer() {
+      new JoinOnComma().apply(null);
+   }
+}
