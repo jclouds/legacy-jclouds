c1637ba7409737d6d949fbb5ce5da334ed38e992
Adrian Cole <adrian@jclouds.org>
2011-03-04 15:45:59 -0500

    added guest os to cloudstack

---
+++ sandbox-apis/cloudstack/src/test/java/org/jclouds/cloudstack/options/ListOSTypesOptionsTest.java
@@ -0,0 +1,57 @@
+/**
+ *
+ * Copyright (C) 2010 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+
+package org.jclouds.cloudstack.options;
+
+import static org.jclouds.cloudstack.options.ListOSTypesOptions.Builder.OSCategoryId;
+import static org.jclouds.cloudstack.options.ListOSTypesOptions.Builder.id;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Tests behavior of {@code ListOSTypesOptions}
+ * 
+ * @author Adrian Cole
+ */
+@Test(groups = "unit")
+public class ListOSTypesOptionsTest {
+
+   public void testId() {
+      ListOSTypesOptions options = new ListOSTypesOptions().id(6);
+      assertEquals(ImmutableList.of("6"), options.buildQueryParameters().get("id"));
+   }
+
+   public void testIdStatic() {
+      ListOSTypesOptions options = id(6);
+      assertEquals(ImmutableList.of("6"), options.buildQueryParameters().get("id"));
+   }
+
+   public void testOSCategoryId() {
+      ListOSTypesOptions options = new ListOSTypesOptions().OSCategoryId(6);
+      assertEquals(ImmutableList.of("6"), options.buildQueryParameters().get("oscategoryid"));
+   }
+
+   public void testOSCategoryIdStatic() {
+      ListOSTypesOptions options = OSCategoryId(6);
+      assertEquals(ImmutableList.of("6"), options.buildQueryParameters().get("oscategoryid"));
+   }
+}
