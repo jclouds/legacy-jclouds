cfb74ffcc139e4ab901994d21fe1ba51c94fb647
Chris Custine <ccustine@apache.org>
2011-03-12 13:28:39 -0700

    Fix for classloader issues in OSGi, web apps, etc.

--- scriptbuilder/src/main/java/org/jclouds/scriptbuilder/util/Utils.java
+++ scriptbuilder/src/main/java/org/jclouds/scriptbuilder/util/Utils.java
@@ -134,8 +134,8 @@ public class Utils {
 
    public static String writeFunctionFromResource(String function, OsFamily family) {
       try {
-         return CharStreams.toString(Resources.newReaderSupplier(Resources.getResource(String
-                  .format("functions/%s.%s", function, ShellToken.SH.to(family))), Charsets.UTF_8));
+         return CharStreams.toString(Resources.newReaderSupplier(Resources.getResource(Utils.class, String
+                  .format("/functions/%s.%s", function, ShellToken.SH.to(family))), Charsets.UTF_8));
       } catch (IOException e) {
          throw new FunctionNotFoundException(function, family, e);
       }
