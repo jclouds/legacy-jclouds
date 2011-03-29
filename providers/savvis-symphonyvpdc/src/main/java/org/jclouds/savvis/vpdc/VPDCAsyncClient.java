7c90225106a92463c15d91e7f014123ef10ce864
Adrian Cole <adrian@jclouds.org>
2011-03-29 13:05:40 -0700

    removed typo in javadoc

--- providers/savvis-symphonyvpdc/src/main/java/org/jclouds/savvis/vpdc/VPDCAsyncClient.java
+++ providers/savvis-symphonyvpdc/src/main/java/org/jclouds/savvis/vpdc/VPDCAsyncClient.java
@@ -66,7 +66,7 @@ public interface VPDCAsyncClient {
    @Delegate
    ServiceManagementAsyncClient getServiceManagementClient();
 
-   /**s
+   /**
     * 
     * @return a listing of all orgs that the current user has access to.
     */
