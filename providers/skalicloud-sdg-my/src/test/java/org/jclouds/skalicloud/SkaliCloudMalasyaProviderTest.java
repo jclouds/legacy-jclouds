d313950f38ff3aa58022fcd2b76a10cb92db129b
Gustavo Morozowski <gustavomzw@gmail.com>
2011-06-02 22:57:07 -0300

    Issue 550: Added ProviderMetaData for SkaliCloud.

---
+++ providers/skalicloud-sdg-my/src/test/java/org/jclouds/skalicloud/SkaliCloudMalasyaProviderTest.java
@@ -0,0 +1,35 @@
+/**
+ *
+ * Copyright (C) 2011 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+package org.jclouds.skalicloud;
+
+import org.jclouds.providers.BaseProviderMetadataTest;
+import org.jclouds.providers.ProviderMetadata;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author Adrian Cole
+ */
+@Test(groups = "unit", testName = "SkaliCloudMalaysiaProviderTest")
+public class SkaliCloudMalasyaProviderTest extends BaseProviderMetadataTest {
+
+   public SkaliCloudMalasyaProviderTest() {
+      super(new SkaliCloudMalaysiaProviderMetadata(), ProviderMetadata.COMPUTE_TYPE);
+   }
+}
