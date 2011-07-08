37e6424040513dac7ba9350ea47e0d9e278edfbd
Adrian Cole <adrian@jclouds.org>
2011-07-07 17:03:32 -0700

    Issue 557:support Virtacore vCloud Express

---
+++ sandbox-providers/virtacore-vcloudexpress/src/main/java/org/jclouds/virtacore/vcloudexpress/config/VirtacoreVCloudExpressRestClientModule.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * Copyright (C) 2011 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+package org.jclouds.virtacore.vcloudexpress.config;
+
+import org.jclouds.http.RequiresHttp;
+import org.jclouds.rest.ConfiguresRestClient;
+import org.jclouds.vcloud.config.VCloudRestClientModule;
+import org.jclouds.vcloud.filters.SetVCloudTokenCookie;
+import org.jclouds.virtacore.vcloudexpress.filters.SetVCloudTokenCookieAndAuthorizationHeader;
+
+/**
+ * 
+ * @author Adrian Cole
+ */
+@RequiresHttp
+@ConfiguresRestClient
+public class VirtacoreVCloudExpressRestClientModule extends VCloudRestClientModule {
+
+   @Override
+   protected void configure() {
+      super.configure();
+      bind(SetVCloudTokenCookie.class).to(SetVCloudTokenCookieAndAuthorizationHeader.class);
+   }
+
+}
