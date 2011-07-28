6ff1738e8a94ed9500f6aa23c28d0144131a80f5
Adrian Cole <adrian@jclouds.org>
2011-07-28 01:37:26 -0700

    Issue 645:support Go2Cloud Johannesburg

---
+++ providers/go2cloud-jhb1/src/main/java/org/jclouds/go2cloud/Go2CloudJohannesburg1PropertiesBuilder.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * Copyright (C) 2011 Cloud Conscious, LLC. <info@cloudconscious.com>
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
+package org.jclouds.go2cloud;
+
+import static org.jclouds.Constants.PROPERTY_API_VERSION;
+import static org.jclouds.Constants.PROPERTY_ENDPOINT;
+import static org.jclouds.Constants.PROPERTY_ISO3166_CODES;
+
+import java.util.Properties;
+
+import org.jclouds.elasticstack.ElasticStackPropertiesBuilder;
+
+/**
+ * 
+ * @author Adrian Cole
+ */
+public class Go2CloudJohannesburg1PropertiesBuilder extends ElasticStackPropertiesBuilder {
+
+   @Override
+   protected Properties defaultProperties() {
+      Properties properties = super.defaultProperties();
+      properties.setProperty(PROPERTY_ISO3166_CODES, "ZA-GP");
+      properties.setProperty(PROPERTY_ENDPOINT, "http://api.jhb1.go2cloud.co.za");
+      properties.setProperty(PROPERTY_API_VERSION, "1.0");
+      return properties;
+   }
+
+   public Go2CloudJohannesburg1PropertiesBuilder(Properties properties) {
+      super(properties);
+   }
+
+}
