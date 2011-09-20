d5df2d2057a04317362bce259ec22cc482ae2ff2
Jason King <jason.king@cloudsoftcorp.com>
2011-09-20 13:32:53 +0100

    Issue 158: Added cancel service

--- sandbox-providers/softlayer/src/main/java/org/jclouds/softlayer/features/VirtualGuestAsyncClient.java
+++ sandbox-providers/softlayer/src/main/java/org/jclouds/softlayer/features/VirtualGuestAsyncClient.java
@@ -114,4 +114,13 @@ public interface VirtualGuestAsyncClient {
    @Consumes(MediaType.APPLICATION_JSON)
    @ExceptionParser(ReturnVoidOnNotFoundOr404.class)
    ListenableFuture<Void> resumeVirtualGuest(@PathParam("id") long id);
+
+   /**
+    * @see VirtualGuestClient#cancelService
+    */
+   @GET
+   @Path("/SoftLayer_Billing_Item/{id}/cancelService.json")
+   @Consumes(MediaType.APPLICATION_JSON)
+   @ExceptionParser(ReturnVoidOnNotFoundOr404.class)
+   ListenableFuture<Boolean> cancelService(@PathParam("id") long id);
 }
