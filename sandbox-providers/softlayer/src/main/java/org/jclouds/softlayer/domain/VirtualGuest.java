392e75fe4155647a6605abfb1969013c2d2cfc6e
Jason King <jason.king@cloudsoftcorp.com>
2011-09-29 10:11:42 +0100

    Issue 158: Added comment about hostname/domain constraints

--- sandbox-providers/softlayer/src/main/java/org/jclouds/softlayer/domain/VirtualGuest.java
+++ sandbox-providers/softlayer/src/main/java/org/jclouds/softlayer/domain/VirtualGuest.java
@@ -31,6 +31,17 @@ import com.google.gson.annotations.SerializedName;
  * interaction. <br/>
  * A guest, also known as a virtual server or CloudLayer Computing Instance, represents an
  * allocation of resources on a virtual host.
+ *
+ * The hostname and domain must be alphanumeric strings that may be separated by periods '.'.
+ * The only other allowable special character is the dash '-'.
+ * However the special characters '.' and '-' may not be consecutive.
+ * Each alphanumeric string separated by a period is considered a label.
+ * Labels must begin and end with an alphanumeric character.
+ * Each label cannot be soley comprised of digits and must be between 1-63 characters in length.
+ * The last label, the TLD (top level domain) must be between 2-6 alphabetic characters.
+ * The domain portion must consist of least one label followed by a period '.' then ending with the TLD label.
+ * Combining the hostname, followed by a period '.', followed by the domain gives the FQDN (fully qualified domain name),
+ * which may not exceed 253 characters in total length.
  * 
  * @author Adrian Cole
  * @see <a href=
