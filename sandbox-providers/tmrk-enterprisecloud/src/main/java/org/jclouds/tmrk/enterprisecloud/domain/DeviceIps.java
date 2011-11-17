99855f9981e41e63a85ccbe069bc9a8e9c469ead
Jason King <jason.king@cloudsoftcorp.com>
2011-11-17 19:58:35 +0000

    Issue 695: Added IpAddress Section of VirtualMachine

---
+++ sandbox-providers/tmrk-enterprisecloud/src/main/java/org/jclouds/tmrk/enterprisecloud/domain/DeviceIps.java
@@ -0,0 +1,70 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.tmrk.enterprisecloud.domain;
+
+import com.google.common.collect.Sets;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Wraps individual IpAddresses
+ * @author Jason King
+ */
+public class DeviceIps {
+
+    private LinkedHashSet<String> ipAddresses = Sets.newLinkedHashSet();
+
+    protected DeviceIps() {
+        // For JAXB
+    }
+
+    @XmlElement(name = "IpAddress")
+    void setIpAddress(String ipAddress) {
+        this.ipAddresses.add(ipAddress);
+    }
+
+    public Set<String> getIpAddresses() {
+        return Collections.unmodifiableSet(ipAddresses);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DeviceIps deviceIps = (DeviceIps) o;
+
+        if (!ipAddresses.equals(deviceIps.ipAddresses)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return ipAddresses.hashCode();
+    }
+
+    public String toString() {
+        return "["+ ipAddresses.toString()+"]";
+    }
+
+}
