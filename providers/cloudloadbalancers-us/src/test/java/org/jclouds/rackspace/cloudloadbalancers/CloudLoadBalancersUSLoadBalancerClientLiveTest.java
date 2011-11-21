e28773337ed97b24c340374a3175556183072dbd
danikov <dan.lobianco@cloudsoftcorp.com>
2011-11-21 14:58:39 +0000

    fixed UK provider/region not working + regression tests

--- providers/cloudloadbalancers-us/src/test/java/org/jclouds/rackspace/cloudloadbalancers/CloudLoadBalancersUSLoadBalancerClientLiveTest.java
+++ providers/cloudloadbalancers-us/src/test/java/org/jclouds/rackspace/cloudloadbalancers/CloudLoadBalancersUSLoadBalancerClientLiveTest.java
@@ -18,7 +18,10 @@
  */
 package org.jclouds.rackspace.cloudloadbalancers;
 
+import static org.testng.Assert.assertEquals;
+
 import org.jclouds.cloudloadbalancers.features.LoadBalancerClientLiveTest;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
 /**
@@ -29,5 +32,12 @@ import org.testng.annotations.Test;
 public class CloudLoadBalancersUSLoadBalancerClientLiveTest extends LoadBalancerClientLiveTest {
    public CloudLoadBalancersUSLoadBalancerClientLiveTest() {
       provider = "cloudloadbalancers-us";
+   }
+   
+   @BeforeGroups(groups = "live")
+   protected void setup() {
+	   super.setup();
+	   assertEquals(client.getConfiguredRegions().size(), 
+			   CloudLoadBalancersUSPropertiesBuilder.REGIONS.length);
    }
 }
