5cd43815af9a5ee1bbd6a0ad6c995516fcdfa81b
Jason King <jason.king@cloudsoftcorp.com>
2011-11-21 13:58:41 +0000

    Issue 695: Refactored domain package: created packages: virtualmachine,hardware,software,network

--- sandbox-providers/tmrk-enterprisecloud/src/main/java/org/jclouds/tmrk/enterprisecloud/domain/hardware/Memory.java
+++ sandbox-providers/tmrk-enterprisecloud/src/main/java/org/jclouds/tmrk/enterprisecloud/domain/hardware/Memory.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.jclouds.tmrk.enterprisecloud.domain;
+package org.jclouds.tmrk.enterprisecloud.domain.hardware;
 
 import org.jclouds.tmrk.enterprisecloud.domain.internal.ResourceCapacity;
 
