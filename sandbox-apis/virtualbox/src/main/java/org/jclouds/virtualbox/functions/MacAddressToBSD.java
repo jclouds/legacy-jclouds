0d6fd403bcf2264b483449c328e110ed0087a95a
Mattias Holmqvist <mattias.holmqvist@gmail.com>
2011-11-23 07:33:56 +0100

    Formatting

--- sandbox-apis/virtualbox/src/main/java/org/jclouds/virtualbox/functions/MacAddressToBSD.java
+++ sandbox-apis/virtualbox/src/main/java/org/jclouds/virtualbox/functions/MacAddressToBSD.java
@@ -31,25 +31,24 @@ import com.google.common.collect.Iterables;
  */
 public enum MacAddressToBSD implements Function<String, String> {
 
-	INSTANCE;
+   INSTANCE;
 
-	@Override
-	public String apply(String macAddress) {
-	      checkArgument(macAddress.length() == 17);
-	   return  Joiner.on(":").join(
-				Iterables.transform(Splitter.on(":").split(macAddress),
-						new Function<String, String>() {
-					@Override
-					public String apply(String arg0) {
-						if (arg0.equals("00"))
-							return "0";
-						if (arg0.startsWith("0"))
-							return arg0.substring(1);
+   @Override
+   public String apply(String macAddress) {
+      checkArgument(macAddress.length() == 17);
+      return Joiner.on(":").join(
+              Iterables.transform(Splitter.on(":").split(macAddress),
+                      new Function<String, String>() {
+                         @Override
+                         public String apply(String arg0) {
+                            if (arg0.equals("00"))
+                               return "0";
+                            if (arg0.startsWith("0"))
+                               return arg0.substring(1);
 
-						return arg0;
-					}
-
-				}));
-	}
+                            return arg0;
+                         }
+                      }));
+   }
 
 }
