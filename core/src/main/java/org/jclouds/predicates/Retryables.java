126cdf6dd506db43dda81f50b6805926f09929da
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2011-12-02 15:17:39 +0000

    added @Beta and source headers as per adrian's comments

--- core/src/main/java/org/jclouds/predicates/Retryables.java
+++ core/src/main/java/org/jclouds/predicates/Retryables.java
@@ -1,9 +1,34 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jclouds.predicates;
 
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.annotations.Beta;
 import com.google.common.base.Predicate;
 
+/** convenience methods to retry application of a predicate, and optionally (reducing quite a bit of boilerplate)
+ * get the final result or throw assertion error
+ * 
+ * @author alex heneveld
+ */
+@Beta
 public class Retryables {
 
    public static <Input> boolean retry(Predicate<Input> predicate, Input input, long maxWaitMillis) {
