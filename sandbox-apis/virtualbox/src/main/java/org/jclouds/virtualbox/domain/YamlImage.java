e46ee9fbc4abed3805578edb8ba27efd45c6c2ec
Adrian Cole <adrian@jclouds.org>
2012-01-09 10:54:57 -0800

    Issue 807:update license headers

--- sandbox-apis/virtualbox/src/main/java/org/jclouds/virtualbox/domain/YamlImage.java
+++ sandbox-apis/virtualbox/src/main/java/org/jclouds/virtualbox/domain/YamlImage.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to jclouds, Inc. (jclouds) under one or more
  * contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,11 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
- * 
- * @author Andrea Turli
  */
-
 package org.jclouds.virtualbox.domain;
 
 import static org.jclouds.compute.util.ComputeServiceUtils.parseOsFamilyOrUnrecognized;
--- sandbox-apis/virtualbox/src/test/java/org/jclouds/virtualbox/predicates/IsLinkedClonesLiveTest.java
+++ sandbox-apis/virtualbox/src/test/java/org/jclouds/virtualbox/predicates/IsLinkedClonesLiveTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to jclouds, Inc. (jclouds) under one or more
  * contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.jclouds.virtualbox.predicates;
 
 import static org.jclouds.virtualbox.config.VirtualBoxConstants.VIRTUALBOX_IMAGE_PREFIX;
