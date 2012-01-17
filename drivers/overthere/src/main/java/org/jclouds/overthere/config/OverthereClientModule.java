47bc14224ff72792dbf43b503511f4e4e6f0f17e
Richard Downer <richard.downer@cloudsoftcorp.com>
2012-01-17 14:57:51 +0000

    Add driver for Overthere
    https://github.com/xebialabs/overthere - a library that does stuff "over there".
    This driver uses overthere's WinRM (Windows Remote Management functionality)
    to be able to perform SshClient operations on Windows hosts.
    
    This work is still in progress.
    
    See http://code.google.com/p/jclouds/issues/detail?id=675

---
+++ drivers/overthere/src/main/java/org/jclouds/overthere/config/OverthereClientModule.java
@@ -0,0 +1,54 @@
+package org.jclouds.overthere.config;
+
+import com.google.common.net.HostAndPort;
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import com.xebialabs.overthere.ConnectionOptions;
+import com.xebialabs.overthere.OperatingSystemFamily;
+import com.xebialabs.overthere.Overthere;
+import com.xebialabs.overthere.OverthereConnection;
+import com.xebialabs.overthere.cifs.CifsConnectionBuilder;
+import com.xebialabs.overthere.cifs.CifsConnectionType;
+import org.jclouds.domain.Credentials;
+import org.jclouds.domain.LoginCredentials;
+import org.jclouds.overthere.OverthereClient;
+import org.jclouds.predicates.InetSocketAddressConnect;
+import org.jclouds.predicates.SocketOpen;
+import org.jclouds.ssh.ConfiguresSshClient;
+import org.jclouds.ssh.SshClient;
+
+/**
+ * Module for the Overthere library for remote access to hosts.
+ *
+ * This is currently biased towards Windows but could theoretically also be
+ * used for Linux sessions too via ssh.
+ *
+ * @author Richard Downer
+ */
+@ConfiguresSshClient
+public class OverthereClientModule extends AbstractModule {
+   @Override
+   protected void configure() {
+      bind(SshClient.Factory.class).to(Factory.class).in(Scopes.SINGLETON);
+      bind(SocketOpen.class).to(InetSocketAddressConnect.class).in(Scopes.SINGLETON);
+   }
+
+   private static class Factory implements SshClient.Factory {
+      @Override
+      public SshClient create(HostAndPort hostAndPort, Credentials credentials) {
+         return create(hostAndPort, LoginCredentials.fromCredentials(credentials));
+      }
+
+      @Override
+      public SshClient create(HostAndPort hostAndPort, LoginCredentials credentials) {
+         ConnectionOptions options = new ConnectionOptions();
+         options.set(ConnectionOptions.ADDRESS, hostAndPort.getHostText());
+         options.set(ConnectionOptions.USERNAME, credentials.getUser());
+         options.set(ConnectionOptions.PASSWORD, credentials.getPassword());
+         options.set(ConnectionOptions.OPERATING_SYSTEM, OperatingSystemFamily.WINDOWS);
+         options.set(CifsConnectionBuilder.CONNECTION_TYPE, CifsConnectionType.WINRM_HTTPS);
+         return new OverthereClient(options);
+      }
+   }
+
+}
