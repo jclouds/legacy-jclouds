47ade5a429afc60ffec858f01a78aca459f2ac4a
Adrian Cole <adrian@jclouds.org>
2012-02-05 23:08:46 -0800

    added doc

--- labs/vcloud-director/src/main/java/org/jclouds/vcloud/director/v1_5/domain/Session.java
+++ labs/vcloud-director/src/main/java/org/jclouds/vcloud/director/v1_5/domain/Session.java
@@ -34,7 +34,9 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
 /**
- * Login Session
+ * Returns a representation of the current session that can serve as a single entry point to the
+ * system, as it provides user, admin, and extension (sysadmin) entry links depending on the
+ * privileges of the current user.
  * 
  * @author Adrian Cole
  */
