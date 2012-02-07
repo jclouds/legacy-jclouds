800bffde9420ad575da6c3e7c146e7c265952965
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2012-02-07 18:48:01 +0000

    Java SimpleDateFormat cannot handle valid ISO8601 time zone strings, fixed

--- core/src/main/java/org/jclouds/date/internal/DateUtils.java
+++ core/src/main/java/org/jclouds/date/internal/DateUtils.java
@@ -27,14 +27,15 @@ import java.util.regex.Pattern;
  */
 public class DateUtils {
 
-   public static final Pattern MILLIS_PATTERN = Pattern.compile("(.*\\.[0-9][0-9][0-9])[0-9]*Z?");
+   public static final Pattern MILLIS_PATTERN = Pattern.compile("(.*\\.[0-9][0-9][0-9])[0-9]*");
 
-   public static final Pattern TZ_PATTERN = Pattern.compile("(.*)[+-][0-9][0-9]:?[0-9][0-9]Z?");
+   // This regexp will match all TZ forms that are valid is ISO 8601
+   public static final Pattern TZ_PATTERN = Pattern.compile("(.*)([+-][0-9][0-9](:?[0-9][0-9])?|Z)");
 
    public static String trimToMillis(String toParse) {
        Matcher matcher = MILLIS_PATTERN.matcher(toParse);
        if (matcher.find()) {
-          toParse = matcher.group(1) + 'Z';
+          toParse = matcher.group(1);
        }
       return toParse;
    }
@@ -44,11 +45,25 @@ public class DateUtils {
    public static String trimTZ(String toParse) {
       Matcher matcher = TZ_PATTERN.matcher(toParse);
       if (matcher.find()) {
-         toParse = matcher.group(1) + 'Z';
+         toParse = matcher.group(1);
       }
+      // TODO explain why this check is here
       if (toParse.length() == 25 && SECOND_PATTERN.matcher(toParse).matches())
-         toParse = toParse.substring(0, toParse.length() - 6) + 'Z';
+         toParse = toParse.substring(0, toParse.length() - 6);
       return toParse;
+   }
+
+   public static String findTZ(String toParse) {
+      Matcher matcher = TZ_PATTERN.matcher(toParse);
+      if (matcher.find()) {
+         // Remove ':' from the TZ string, as SimpleDateFormat can't handle it
+         String tz = matcher.group(2).replace(":", "");
+         // Append '00; if we only have a two digit TZ, as SimpleDateFormat
+         if (tz.length() == 2) tz += "00";
+         return tz;
+      } else {
+         return "";
+      }
    }
 
 }
