473126e7ec088abcf7c27476d58b4c8e7ee06131
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2012-02-20 11:47:35 +0000

    Add numeric ids to Check error messages

--- labs/vcloud-director/src/test/java/org/jclouds/vcloud/director/v1_5/VCloudDirectorLiveTestConstants.java
+++ labs/vcloud-director/src/test/java/org/jclouds/vcloud/director/v1_5/VCloudDirectorLiveTestConstants.java
@@ -23,6 +23,8 @@ package org.jclouds.vcloud.director.v1_5;
  */
 public class VCloudDirectorLiveTestConstants {
 
+   /* danikov */
+
    public static final String REF_REQ_LIVE = "%s reference required to perform live tests";
    public static final String OBJ_REQ_LIVE = "%s instance required to perform live tests";
    public static final String OBJ_FIELD_REQ_LIVE = "%s must have a non-null \"%s\" to perform live tests";
@@ -37,23 +39,24 @@ public class VCloudDirectorLiveTestConstants {
    public static final String OBJ_DEL = "%s (%s) should have been deleted";
    public static final String TASK_COMPLETE_TIMELY = "Task %s should complete in a timely fashion";
    
-   @Deprecated
-   public static final String NOT_NULL_OBJECT_FMT = "The %s field of the %s must not be null";
+   /* grkvlt */
+
+   public static final String NOT_NULL_OBJECT_FMT = "ERR-01: The %s field of the %s must not be null";
    
-   public static final String NOT_EMPTY_OBJECT_FMT = "One or more %s fields of the %s must be present";
+   public static final String NOT_EMPTY_OBJECT_FMT = "ERR-02: One or more %s fields of the %s must be present";
    
-   public static final String REQUIRED_VALUE_OBJECT_FMT = "The %s field of the %s must not be '%s'";
+   public static final String REQUIRED_VALUE_OBJECT_FMT = "ERR-03: The %s field of the %s must not be '%s'; allowed values: %s";
 
-   public static final String REQUIRED_VALUE_FMT = "The %s field must not be '%s'";
+   public static final String REQUIRED_VALUE_FMT = "ERR-04: The %s field must not be '%sr'; allowed values: %s";
 
-   public static final String MUST_BE_WELL_FORMED_FMT = "The %s field must be well formed: '%s'";
+   public static final String MUST_BE_WELL_FORMED_FMT = "ERR-05: The %s field must be well formed: '%s'";
 
-   public static final String MUST_EXIST_FMT = "The '%s' %s must exist";
+   public static final String MUST_EXIST_FMT = "ERR-06: The '%s' %s must exist";
    
-   public static final String MUST_CONTAIN_FMT = "The %s field must contain '%s': '%s'";
+   public static final String MUST_CONTAIN_FMT = "ERR-07: The %s field must contain '%s': '%s'";
 
-   public static final String CONDITION_FMT = "The %s field must be %s: '%s'";
+   public static final String CONDITION_FMT = "ERR-08: The %s field must be %s: '%s'";
 
-   public static final String CORRECT_VALUE_OBJECT_FMT = "The %s field of the %s must be '%s': '%s'";
+   public static final String CORRECT_VALUE_OBJECT_FMT = "ERR-09: The %s field of the %s must be '%s': '%s'";
 
 }
