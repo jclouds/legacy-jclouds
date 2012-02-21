06ca9b1e3d8662164d834a788becfda0c770b39d
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2012-02-21 16:49:56 +0000

    Updated our error codes

--- labs/vcloud-director/src/test/java/org/jclouds/vcloud/director/v1_5/VCloudDirectorLiveTestConstants.java
+++ labs/vcloud-director/src/test/java/org/jclouds/vcloud/director/v1_5/VCloudDirectorLiveTestConstants.java
@@ -23,40 +23,52 @@ package org.jclouds.vcloud.director.v1_5;
  */
 public class VCloudDirectorLiveTestConstants {
 
-   /* danikov */
+   /* Error codes from 100 to 199 reflect parsing and other errors in domain objects. */
 
-   public static final String REF_REQ_LIVE = "%s reference required to perform live tests";
-   public static final String OBJ_REQ_LIVE = "%s instance required to perform live tests";
-   public static final String OBJ_FIELD_REQ_LIVE = "%s must have a non-null \"%s\" to perform live tests";
-   public static final String OBJ_FIELD_REQ = "%s must always have a non-null field \"%s\"";
-   public static final String OBJ_FIELD_ATTRB_REQ = "%s %s (%s) must always have a non-null field \"%s\"";
-   public static final String OBJ_FIELD_EQ = "%s %s must have the value \"%s\" (%s)";
-   public static final String OBJ_FIELD_CONTAINS = "%s %s must contain the values \"%s\" (%s)";
-   public static final String OBJ_FIELD_GTE_0 = "%s field %s must be greater than to equal to 0 (%d)";
-   public static final String GETTER_RETURNS_SAME_OBJ = "%s should return the same %s as %s (%s, %s)";
-   public static final String OBJ_FIELD_UPDATABLE = "%s field %s should be updatable";
-   public static final String OBJ_FIELD_ATTRB_DEL = "%s %s (%s) should have deleted field \"%s\" (%s)";
-   public static final String OBJ_DEL = "%s (%s) should have been deleted";
-   public static final String TASK_COMPLETE_TIMELY = "Task %s should complete in a timely fashion";
+   public static final String OK = "ERR-100: ok";
+
+   public static final String REF_REQ_LIVE = "ERR-101: %s reference required to perform live tests";
+
+   public static final String OBJ_REQ_LIVE = "ERR-102: %s instance required to perform live tests";
+
+   public static final String OBJ_FIELD_REQ_LIVE = "ERR-103: %s must have a non-null \"%s\" to perform live tests";
+
+   public static final String OBJ_FIELD_REQ = "ERR-103: %s must always have a non-null field \"%s\"";
+
+   public static final String OBJ_FIELD_ATTRB_REQ = "ERR-105: %s %s (%s) must always have a non-null field \"%s\"";
+
+   public static final String OBJ_FIELD_EQ = "ERR-106: %s %s must have the value \"%s\" (%s)";
+
+   public static final String OBJ_FIELD_CONTAINS = "ERR-107: %s %s must contain the values \"%s\" (%s)";
+
+   public static final String OBJ_FIELD_GTE_0 = "ERR-108: %s field %s must be greater than to equal to 0 (%d)";
+
+   public static final String GETTER_RETURNS_SAME_OBJ = "ERR-109: %s should return the same %s as %s (%s, %s)";
+
+   public static final String OBJ_FIELD_UPDATABLE = "ERR-110: %s field %s should be updatable";
+
+   public static final String OBJ_FIELD_ATTRB_DEL = "ERR-111: %s %s (%s) should have deleted field \"%s\" (%s)";
+
+   public static final String OBJ_DEL = "ERR-112: %s (%s) should have been deleted";
+
+   public static final String TASK_COMPLETE_TIMELY = "ERR-113: Task %s should complete in a timely fashion";
+
+   public static final String NOT_NULL_OBJECT_FMT = "ERR-114: The %s field of the %s must not be null";
    
-   /* grkvlt */
-
-   public static final String NOT_NULL_OBJECT_FMT = "ERR-01: The %s field of the %s must not be null";
+   public static final String NOT_EMPTY_OBJECT_FMT = "ERR-115: One or more %s fields of the %s must be present";
    
-   public static final String NOT_EMPTY_OBJECT_FMT = "ERR-02: One or more %s fields of the %s must be present";
+   public static final String REQUIRED_VALUE_OBJECT_FMT = "ERR-116: The %s field of the %s must not be '%s'; allowed values: %s";
+
+   public static final String REQUIRED_VALUE_FMT = "ERR-117: The %s field must not be '%s'; allowed values: %s";
+
+   public static final String MUST_BE_WELL_FORMED_FMT = "ERR-118: The %s field must be well formed: '%s'";
+
+   public static final String MUST_EXIST_FMT = "ERR-119: The '%s' %s must exist";
    
-   public static final String REQUIRED_VALUE_OBJECT_FMT = "ERR-03: The %s field of the %s must not be '%s'; allowed values: %s";
+   public static final String MUST_CONTAIN_FMT = "ERR-120: The %s field must contain '%s': '%s'";
 
-   public static final String REQUIRED_VALUE_FMT = "ERR-04: The %s field must not be '%s'; allowed values: %s";
+   public static final String CONDITION_FMT = "ERR-121: The %s field must be %s: '%s'";
 
-   public static final String MUST_BE_WELL_FORMED_FMT = "ERR-05: The %s field must be well formed: '%s'";
-
-   public static final String MUST_EXIST_FMT = "ERR-06: The '%s' %s must exist";
-   
-   public static final String MUST_CONTAIN_FMT = "ERR-07: The %s field must contain '%s': '%s'";
-
-   public static final String CONDITION_FMT = "ERR-08: The %s field must be %s: '%s'";
-
-   public static final String CORRECT_VALUE_OBJECT_FMT = "ERR-09: The %s field of the %s must be '%s': '%s'";
+   public static final String CORRECT_VALUE_OBJECT_FMT = "ERR-122: The %s field of the %s must be '%s': '%s'";
 
 }
