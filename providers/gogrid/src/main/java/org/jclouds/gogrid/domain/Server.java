cf0b725b26cb29cbeb9eb346922e1ac964b7e56b
Adrian Cole <adrian@jclouds.org>
2012-03-07 12:59:58 -0800

    refactored hashCode, etc. as in NL sometimes date fields are not present

--- providers/gogrid/src/main/java/org/jclouds/gogrid/domain/Server.java
+++ providers/gogrid/src/main/java/org/jclouds/gogrid/domain/Server.java
@@ -18,7 +18,10 @@
  */
 package org.jclouds.gogrid.domain;
 
+import static com.google.common.base.Objects.equal;
+import static com.google.common.base.Objects.toStringHelper;
 
+import com.google.common.base.Objects;
 import com.google.common.primitives.Longs;
 
 /**
@@ -46,7 +49,7 @@ public class Server implements Comparable<Server> {
    }
 
    public Server(long id, Option datacenter, boolean sandbox, String name, String description, ServerState state,
-         Option type, Option ram, Option os, Ip ip, ServerImage image) {
+            Option type, Option ram, Option os, Ip ip, ServerImage image) {
       this.id = id;
       this.isSandbox = sandbox;
       this.name = name;
@@ -105,93 +108,37 @@ public class Server implements Comparable<Server> {
    }
 
    @Override
-   public boolean equals(Object obj) {
-      if (this == obj)
+   public boolean equals(Object object) {
+      if (this == object) {
          return true;
-      if (obj == null)
+      }
+      if (object instanceof Server) {
+         final Server other = Server.class.cast(object);
+         return equal(id, other.id) && equal(name, other.name);
+      } else {
          return false;
-      if (getClass() != obj.getClass())
-         return false;
-      Server other = (Server) obj;
-      if (datacenter == null) {
-         if (other.datacenter != null)
-            return false;
-      } else if (!datacenter.equals(other.datacenter))
-         return false;
-      if (description == null) {
-         if (other.description != null)
-            return false;
-      } else if (!description.equals(other.description))
-         return false;
-      if (id != other.id)
-         return false;
-      if (image == null) {
-         if (other.image != null)
-            return false;
-      } else if (!image.equals(other.image))
-         return false;
-      if (ip == null) {
-         if (other.ip != null)
-            return false;
-      } else if (!ip.equals(other.ip))
-         return false;
-      if (isSandbox != other.isSandbox)
-         return false;
-      if (name == null) {
-         if (other.name != null)
-            return false;
-      } else if (!name.equals(other.name))
-         return false;
-      if (os == null) {
-         if (other.os != null)
-            return false;
-      } else if (!os.equals(other.os))
-         return false;
-      if (ram == null) {
-         if (other.ram != null)
-            return false;
-      } else if (!ram.equals(other.ram))
-         return false;
-      if (state == null) {
-         if (other.state != null)
-            return false;
-      } else if (!state.equals(other.state))
-         return false;
-      if (type == null) {
-         if (other.type != null)
-            return false;
-      } else if (!type.equals(other.type))
-         return false;
-      return true;
+      }
    }
 
    @Override
    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((datacenter == null) ? 0 : datacenter.hashCode());
-      result = prime * result + ((description == null) ? 0 : description.hashCode());
-      result = prime * result + (int) (id ^ (id >>> 32));
-      result = prime * result + ((image == null) ? 0 : image.hashCode());
-      result = prime * result + ((ip == null) ? 0 : ip.hashCode());
-      result = prime * result + (isSandbox ? 1231 : 1237);
-      result = prime * result + ((name == null) ? 0 : name.hashCode());
-      result = prime * result + ((os == null) ? 0 : os.hashCode());
-      result = prime * result + ((ram == null) ? 0 : ram.hashCode());
-      result = prime * result + ((state == null) ? 0 : state.hashCode());
-      result = prime * result + ((type == null) ? 0 : type.hashCode());
-      return result;
+      return Objects.hashCode(id, name);
    }
 
    @Override
-   public int compareTo(Server o) {
-      return Longs.compare(id, o.getId());
+   public int compareTo(Server that) {
+      if (that == null)
+         return 1;
+      if (this == that)
+         return 0;
+      return Longs.compare(id, that.getId());
    }
 
    @Override
    public String toString() {
-      return "Server [datacenter=" + datacenter + ", description=" + description + ", id=" + id + ", image=" + image
-            + ", ip=" + ip + ", isSandbox=" + isSandbox + ", name=" + name + ", os=" + os + ", ram=" + ram + ", state="
-            + state + ", type=" + type + "]";
+      return toStringHelper("").add("id", id).add("name", name).add("description", description).add("os", os).add(
+               "image", image).add("datacenter", datacenter).add("state", state).add("ip", ip).add("isSandbox",
+               isSandbox).add("ram", ram).add("type", type).toString();
    }
+
 }
