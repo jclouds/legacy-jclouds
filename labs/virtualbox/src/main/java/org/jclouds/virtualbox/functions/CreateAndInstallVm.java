1036c7a0f143ad4f9086a8893c2c3d0793927ad5
David Ribeiro Alves <davidralves@gmail.com>
2012-03-14 23:55:55 +0000

    fixed broken master creation because guestAdditions was not executed.fixed but related to qemu (https://bugs.launchpad.net/ubuntu/+source/qemu/+bug/732864) where a lock file (/etc/gshadow.lock) on the master prevents clones from adding users. AdminAccess should work now

--- labs/virtualbox/src/main/java/org/jclouds/virtualbox/functions/CreateAndInstallVm.java
+++ labs/virtualbox/src/main/java/org/jclouds/virtualbox/functions/CreateAndInstallVm.java
@@ -121,18 +121,18 @@ public class CreateAndInstallVm implements Function<MasterSpec, IMachine> {
 				"timed out waiting for guest %s to be accessible via ssh",
 				vmName);
 
-		//logger.debug(">> awaiting installation of guest additions on vm: %s", vmName);
-		//checkState(guestAdditionsInstaller.apply(vm));
+		logger.debug(">> awaiting installation of guest additions on vm: %s", vmName);
+		checkState(guestAdditionsInstaller.apply(vm));
 
 		logger.debug(">> awaiting post-installation actions on vm: %s", vmName);
 
-		NodeMetadata vmMetadata = imachineToNodeMetadata.apply(vm);
+//		NodeMetadata vmMetadata = imachineToNodeMetadata.apply(vm);
 
-		ListenableFuture<ExecResponse> execFuture =
-		machineUtils.runScriptOnNode(vmMetadata, call("cleanupUdevIfNeeded"), RunScriptOptions.NONE);
+//		ListenableFuture<ExecResponse> execFuture =
+//		machineUtils.runScriptOnNode(vmMetadata, call("cleanupUdevIfNeeded"), RunScriptOptions.NONE);
 
-		ExecResponse execResponse = Futures.getUnchecked(execFuture);
-		checkState(execResponse.getExitStatus() == 0);
+//		ExecResponse execResponse = Futures.getUnchecked(execFuture);
+//		checkState(execResponse.getExitStatus() == 0);
 
 		logger.debug(
 				"<< installation of image complete. Powering down node(%s)",
