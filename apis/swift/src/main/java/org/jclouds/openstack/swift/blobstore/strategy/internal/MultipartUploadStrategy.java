1d5c0ed5ebded9c4d69a4f05967fa4b960665c6a
Roman Bogorodskiy <bogorodskiy@gmail.com>
2012-03-29 10:22:46 +0400

    Stubs for multipart upload support in swift.

---
+++ apis/swift/src/main/java/org/jclouds/openstack/swift/blobstore/strategy/internal/MultipartUploadStrategy.java
@@ -0,0 +1,12 @@
+package org.jclouds.openstack.swift.blobstore.strategy.internal;
+
+import com.google.inject.ImplementedBy;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.blobstore.options.PutOptions;
+import org.jclouds.openstack.swift.blobstore.strategy.MultipartUpload;
+
+@ImplementedBy(SequentialMultipartUploadStrategy.class)
+public interface MultipartUploadStrategy extends MultipartUpload {
+
+    String execute(String container, Blob blob, PutOptions options);
+}
