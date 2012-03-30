f7de4b9df456cb3802f7bde6ea84b5b390ffe260
Adrian Cole <adrian@jclouds.org>
2012-03-30 16:57:31 -0700

    Issue 891: switch to openjdk; tested on ubuntu

--- compute/src/test/java/org/jclouds/compute/BaseComputeServiceLiveTest.java
+++ compute/src/test/java/org/jclouds/compute/BaseComputeServiceLiveTest.java
@@ -208,7 +208,7 @@ public abstract class BaseComputeServiceLiveTest extends BaseVersionedServiceLiv
 
    @Test(enabled = true, expectedExceptions = NoSuchElementException.class)
    public void testCorrectExceptionRunningNodesNotFound() throws Exception {
-      client.runScriptOnNodesMatching(runningInGroup("zebras-are-awesome"), InstallJDK.fromURL());
+      client.runScriptOnNodesMatching(runningInGroup("zebras-are-awesome"), InstallJDK.fromOpenJDK());
    }
 
    // since surefire and eclipse don't otherwise guarantee the order, we are
@@ -370,7 +370,7 @@ public abstract class BaseComputeServiceLiveTest extends BaseVersionedServiceLiv
 
    protected static Template addRunScriptToTemplate(Template template) {
       template.getOptions().runScript(
-            Statements.newStatementList(AdminAccess.standard(), InstallJDK.fromURL()));
+            Statements.newStatementList(AdminAccess.standard(), InstallJDK.fromOpenJDK()));
       return template;
    }
 
@@ -434,7 +434,7 @@ public abstract class BaseComputeServiceLiveTest extends BaseVersionedServiceLiv
 
    protected Map<? extends NodeMetadata, ExecResponse> runScriptWithCreds(final String group, OperatingSystem os,
          LoginCredentials creds) throws RunScriptOnNodesException {
-      return client.runScriptOnNodesMatching(runningInGroup(group), InstallJDK.fromURL(), overrideLoginCredentials(creds)
+      return client.runScriptOnNodesMatching(runningInGroup(group), InstallJDK.fromOpenJDK(), overrideLoginCredentials(creds)
             .nameTask("runScriptWithCreds"));
    }
 
---
+++ scriptbuilder/src/main/resources/functions/installOpenJDK.sh
@@ -0,0 +1,35 @@
+function setupJavaHomeInProfile() {
+  test -n \"$SUDO_USER\" && cat >> `getent passwd $SUDO_USER| cut -f6 -d:`/.bashrc <<-'END_OF_JCLOUDS_FILE'
+	export JAVA_HOME=/usr/local/jdk
+	export PATH=$JAVA_HOME/bin:$PATH
+END_OF_JCLOUDS_FILE
+  cat >> /etc/bashrc <<-'END_OF_JCLOUDS_FILE'
+	export JAVA_HOME=/usr/local/jdk
+	export PATH=$JAVA_HOME/bin:$PATH
+END_OF_JCLOUDS_FILE
+  cat >> $HOME/.bashrc <<-'END_OF_JCLOUDS_FILE'
+	export JAVA_HOME=/usr/local/jdk
+	export PATH=$JAVA_HOME/bin:$PATH
+END_OF_JCLOUDS_FILE
+  cat >> /etc/skel/.bashrc <<-'END_OF_JCLOUDS_FILE'
+	export JAVA_HOME=/usr/local/jdk
+	export PATH=$JAVA_HOME/bin:$PATH
+END_OF_JCLOUDS_FILE
+}
+
+function installJDK() {
+  if hash apt-get 2>/dev/null; then
+    export pkg=openjdk-7-jdk
+    apt-get-install $pkg || ( apt-get-upgrade && apt-get-install $pkg )
+  elif hash yum 2>/dev/null; then
+    export pkg=java-1.7.0-openjdk
+    yum --nogpgcheck -y ensure $pkg
+  else
+    abort "we only support apt-get and yum right now... please contribute!"
+    return 1
+  fi
+  JAVA_HOME=`ls -d /usr/lib/jvm/java-7-openjdk-*|grep -v common`
+  ln -Fs $JAVA_HOME /usr/local/jdk 
+  /usr/local/jdk/bin/java -version || abort "cannot run java"
+  setupJavaHomeInProfile
+}
