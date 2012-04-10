6aab0d771668bc6b0a88c874db0943dc334ea887
Ignasi Barrera <ignasi.barrera@abiquo.com>
2012-04-10 15:50:43 +0200

    Added EventBus module to configure the sync and async EventBus

---
+++ core/src/main/java/org/jclouds/events/config/annotations/AsyncBus.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jclouds.events.config.annotations;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+import org.jclouds.events.config.EventBusModule;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+
+/**
+ * Used to configure {@link EventBus} injection, providing a flexible way to inject the
+ * {@link AsyncEventBus}.
+ * 
+ * @author Ignasi Barrera
+ * 
+ * @see EventBusModule
+ */
+@Target({ANNOTATION_TYPE, FIELD, PARAMETER})
+@Retention(RUNTIME)
+@Qualifier
+public @interface AsyncBus {
+
+}
