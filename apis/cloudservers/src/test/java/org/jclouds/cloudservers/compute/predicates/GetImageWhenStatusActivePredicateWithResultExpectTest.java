fc159d1686328984f6fd420e1659af9ab855e584
David Ribeiro Alves <davidralves@gmail.com>
2012-05-10 07:33:53 +0100

    cloudservers imageextension expect tests are passing

--- apis/cloudservers/src/test/java/org/jclouds/cloudservers/compute/predicates/GetImageWhenStatusActivePredicateWithResultExpectTest.java
+++ apis/cloudservers/src/test/java/org/jclouds/cloudservers/compute/predicates/GetImageWhenStatusActivePredicateWithResultExpectTest.java
@@ -1,71 +1,91 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jclouds.cloudservers.compute.predicates;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
-import static org.jclouds.location.reference.LocationConstants.PROPERTY_REGIONS;
 
 import java.net.URI;
 import java.util.Map;
-import java.util.Properties;
 
-import org.jclouds.apis.ApiMetadata;
-import org.jclouds.cloudservers.CloudServersApiMetadata;
+import org.jclouds.cloudservers.internal.BaseCloudServersComputeServiceExpectTest;
 import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.domain.Image;
 import org.jclouds.http.HttpRequest;
 import org.jclouds.http.HttpResponse;
-import org.jclouds.openstack.keystone.v1_1.internal.BaseKeystoneRestClientExpectTest;
 import org.jclouds.predicates.PredicateWithResult;
 import org.testng.annotations.Test;
 
-import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.net.HttpHeaders;
 import com.google.inject.Injector;
-import com.google.inject.Module;
 
+/**
+ * 
+ * @author David Alves
+ * 
+ */
 @Test(groups = "unit", testName = "GetImageWhenStatusActivePredicateWithResultExpectTest")
-public class GetImageWhenStatusActivePredicateWithResultExpectTest extends BaseKeystoneRestClientExpectTest<Injector>
-         implements Function<ComputeServiceContext, Injector> {
+public class GetImageWhenStatusActivePredicateWithResultExpectTest extends
+         BaseCloudServersComputeServiceExpectTest<Injector> {
 
    private final HttpRequest listImagesDetail = HttpRequest
             .builder()
             .method("GET")
-            .endpoint(URI.create("https://lon.servers.api.rackspacecloud.com/v1.0/10001786/images/detail?format=json"))
-            .headers(ImmutableMultimap.<String, String> builder().put("X-Auth-Token", authToken)
-                     .put(HttpHeaders.ACCEPT, "application/json").build()).build();
+            .endpoint(
+                     URI.create("https://lon.servers.api.rackspacecloud.com/v1.0/10001786/images/detail?format=json&now=1257695648897"))
+            .headers(ImmutableMultimap.<String, String> builder().put(HttpHeaders.ACCEPT, "application/json")
+                     .put("X-Auth-Token", authToken).build()).build();
 
    private final HttpResponse listImagesResponse = HttpResponse.builder().statusCode(200)
             .payload(payloadFromResource("/test_list_images_detail_imageextension.json")).build();
 
-   Map<HttpRequest, HttpResponse> requestResponseMap = ImmutableMap.<HttpRequest, HttpResponse> builder()
-            .put(initialAuth, responseWithAuth).put(listImagesDetail, listImagesResponse).build();
+   private final Map<HttpRequest, HttpResponse> requestResponseMap = ImmutableMap.<HttpRequest, HttpResponse> builder()
+            .put(listImagesDetail, listImagesResponse).put(initialAuth, responseWithAuth).build();
 
-   public GetImageWhenStatusActivePredicateWithResultExpectTest() {
-      provider = "cloudservers";
+   public void testReturnsFalseOnQueuedAndSavingAndTrueOnActive() {
+      Injector injector = requestsSendResponses(requestResponseMap);
+      PredicateWithResult<Integer, Image> predicate = injector
+               .getInstance(GetImageWhenStatusActivePredicateWithResult.class);
+      assertTrue(predicate.apply(2));
+      assertFalse(predicate.apply(743));
+      assertFalse(predicate.apply(744));
    }
 
-   @Override
-   protected ApiMetadata createApiMetadata() {
-      return new CloudServersApiMetadata();
+   public void testFailsOnOtherStatuses() {
+      Injector injector = requestsSendResponses(requestResponseMap);
+      PredicateWithResult<Integer, Image> predicate = injector
+               .getInstance(GetImageWhenStatusActivePredicateWithResult.class);
+      assertTrue(illegalStateExceptionThrown(predicate, 745));
+      assertTrue(illegalStateExceptionThrown(predicate, 746));
+      assertTrue(illegalStateExceptionThrown(predicate, 747));
+      assertTrue(illegalStateExceptionThrown(predicate, 748));
    }
 
-   @Override
-   protected Properties setupProperties() {
-      Properties overrides = new Properties();
-      overrides.setProperty(PROPERTY_REGIONS, "US");
-      overrides.setProperty(provider + ".endpoint", endpoint);
-      return overrides;
-   }
-
-   @Override
-   public Injector createClient(Function<HttpRequest, HttpResponse> fn, Module module, Properties props) {
-      return apply(createComputeServiceContext(fn, module, props));
-   }
-
-   private ComputeServiceContext createComputeServiceContext(Function<HttpRequest, HttpResponse> fn, Module module,
-            Properties props) {
-      return createInjector(fn, module, props).getInstance(ComputeServiceContext.class);
+   private boolean illegalStateExceptionThrown(PredicateWithResult<Integer, Image> predicate, Integer id) {
+      try {
+         predicate.apply(id);
+      } catch (IllegalStateException e) {
+         return true;
+      }
+      return false;
    }
 
    @Override
@@ -73,10 +93,4 @@ public class GetImageWhenStatusActivePredicateWithResultExpectTest extends BaseK
       return input.utils().injector();
    }
 
-   public void testReturnsFalseOnQueuedAndSavingAndTrueOnActive() {
-      Injector injector = requestsSendResponses(requestResponseMap);
-      PredicateWithResult<Integer, Image> predicate = injector
-               .getInstance(GetImageWhenStatusActivePredicateWithResult.class);
-      assertTrue(predicate.apply(2));
-   }
 }
