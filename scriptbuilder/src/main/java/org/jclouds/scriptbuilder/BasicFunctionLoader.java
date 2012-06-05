65ecc8d0855a9e289fb90d74b6f4dc1108d2f45e
Ioannis Canellos <iocanel@apache.org>
2012-06-05 13:36:10 +0300

    Added a FunctionLoader interfaces for loading functions and also added a basic implementation and an OSGi implementation.

---
+++ scriptbuilder/src/main/java/org/jclouds/scriptbuilder/BasicFunctionLoader.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.scriptbuilder;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Resources;
+import org.jclouds.scriptbuilder.domain.OsFamily;
+import org.jclouds.scriptbuilder.domain.ShellToken;
+import org.jclouds.util.ClassLoadingUtils;
+
+import java.io.IOException;
+
+/**
+ * A {@link FunctionLoader} implementation which loads the target function from the classpath.
+ */
+public class BasicFunctionLoader implements FunctionLoader {
+
+  /**
+   * Loads a function from the classpath using the current or the Thread Context Class Loader.
+   * @param function The function name to load.
+   * @param family   This operating system family of the function.
+   * @return         The function as {@link String}
+   * @throws FunctionNotFoundException
+   */
+  @Override
+  public String loadFunction(String function, OsFamily family) throws FunctionNotFoundException {
+    try {
+      return CharStreams.toString(Resources.newReaderSupplier(ClassLoadingUtils.loadResource(BasicFunctionLoader.class, String
+              .format("/functions/%s.%s", function, ShellToken.SH.to(family))), Charsets.UTF_8));
+    } catch (IOException e) {
+      throw new FunctionNotFoundException(function, family, e);
+    }
+  }
+}
---
+++ scriptbuilder/src/main/java/org/jclouds/scriptbuilder/osgi/ServiceFunctionLoader.java
@@ -0,0 +1,73 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.scriptbuilder.osgi;
+
+import com.google.common.base.Strings;
+import org.jclouds.scriptbuilder.FunctionLoader;
+import org.jclouds.scriptbuilder.FunctionNotFoundException;
+import org.jclouds.scriptbuilder.domain.OsFamily;
+import org.jclouds.scriptbuilder.domain.ShellToken;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * A {@link FunctionLoader} implementation which delegates loading to the OSGi service registry.
+ */
+public class ServiceFunctionLoader implements FunctionLoader {
+
+  private final BundleContext bundleContext;
+
+  public ServiceFunctionLoader(BundleContext bundleContext) {
+    this.bundleContext = bundleContext;
+  }
+
+  /**
+   * Looks up the service registry for an applicable {@link FunctionLoader} and delegates to it.
+   * @param function The function name to load.
+   * @param family   This operating system family of the function.
+   * @return         The function as {@link String}
+   * @throws FunctionNotFoundException
+   */
+  @Override
+  public String loadFunction(String function, OsFamily family) throws FunctionNotFoundException {
+    ServiceReference[] references = null;
+    String filter = String.format("(function=*%s.%s*)", function, ShellToken.SH.to(family));
+    try {
+      references = bundleContext.getServiceReferences(FunctionLoader.class.getName(), filter);
+      for (ServiceReference reference : references) {
+        FunctionLoader loader = (FunctionLoader) bundleContext.getService(reference);
+        String f = loader.loadFunction(function, family);
+        if (!Strings.isNullOrEmpty(f)) {
+          return f;
+        }
+      }
+    } catch (InvalidSyntaxException e) {
+      throw new FunctionNotFoundException(function, family, e);
+    } finally {
+      if (references != null) {
+        for (ServiceReference reference : references) {
+          bundleContext.ungetService(reference);
+        }
+      }
+    }
+    throw new FunctionNotFoundException(function, family);
+  }
+
+}
