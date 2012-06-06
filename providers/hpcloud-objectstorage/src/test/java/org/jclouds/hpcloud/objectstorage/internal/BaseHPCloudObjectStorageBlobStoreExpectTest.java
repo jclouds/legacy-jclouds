8684e5902570260d6d03a6fd1c036138d5753ed2
Adrian Cole <adrian@jclouds.org>
2012-06-06 16:04:06 -0700

    fixed hpcloud-objectstorage blob signing

--- providers/hpcloud-objectstorage/src/test/java/org/jclouds/hpcloud/objectstorage/internal/BaseHPCloudObjectStorageBlobStoreExpectTest.java
+++ providers/hpcloud-objectstorage/src/test/java/org/jclouds/hpcloud/objectstorage/internal/BaseHPCloudObjectStorageBlobStoreExpectTest.java
@@ -1,14 +1,26 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jclouds.hpcloud.objectstorage.internal;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-
-import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 import org.jclouds.blobstore.BlobStore;
-import org.jclouds.domain.Location;
 import org.jclouds.hpcloud.objectstorage.HPCloudObjectStorageProviderMetadata;
 import org.jclouds.http.HttpRequest;
 import org.jclouds.http.HttpResponse;
@@ -17,11 +29,10 @@ import org.jclouds.rest.internal.BaseRestClientExpectTest;
 import org.testng.annotations.Test;
 
 import com.google.common.base.Function;
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.Module;
 
 @Test(groups = "unit", testName = "HPCloudObjectStorageExpectTest")
-public class HPCloudObjectStorageExpectTest extends BaseRestClientExpectTest<BlobStore> {
+public class BaseHPCloudObjectStorageBlobStoreExpectTest extends BaseRestClientExpectTest<BlobStore> {
 
 
     protected HttpRequest keystoneAuthWithUsernameAndPassword;
@@ -29,7 +40,7 @@ public class HPCloudObjectStorageExpectTest extends BaseRestClientExpectTest<Blo
     protected String authToken;
     protected HttpResponse responseWithKeystoneAccess;
 
-    public HPCloudObjectStorageExpectTest() {
+    public BaseHPCloudObjectStorageBlobStoreExpectTest() {
         provider = "hpcloud-objectstorage";
         keystoneAuthWithUsernameAndPassword = KeystoneFixture.INSTANCE.initialAuthWithUsernameAndPassword(identity,
                 credential);
@@ -39,20 +50,6 @@ public class HPCloudObjectStorageExpectTest extends BaseRestClientExpectTest<Blo
         responseWithKeystoneAccess = KeystoneFixture.INSTANCE.responseWithAccess();
 
         identity = KeystoneFixture.INSTANCE.getTenantName() + ":" + identity;
-    }
-
-
-    public void testListObjectsWhenResponseIs2xx() throws Exception {
-        Map<HttpRequest, HttpResponse> requestResponseMap = ImmutableMap.<HttpRequest, HttpResponse> builder().put(
-                keystoneAuthWithAccessKeyAndSecretKey, responseWithKeystoneAccess).build();
-
-        BlobStore clientWhenServersExist = requestsSendResponses(requestResponseMap);
-
-        Set<? extends Location> locations = clientWhenServersExist.listAssignableLocations();
-        assertNotNull(locations);
-        assertEquals(locations.size(), 1);
-        // TODO: does this location make sense?
-        assertEquals(locations.iterator().next().getId(), "hpcloud-objectstorage");
     }
     
     @Override
