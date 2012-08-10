faa3f7ff45abcacad248ef2ce37c62501668855a
Adrian Cole <adrian@jclouds.org>
2012-08-09 17:53:18 -0700

    added epel repo dep to git install

--- scriptbuilder/src/test/resources/test_install_git_scriptbuilder.sh
+++ scriptbuilder/src/test/resources/test_install_git_scriptbuilder.sh
@@ -138,6 +138,16 @@ function installGit() {
   if which dpkg &> /dev/null; then
     ensure_cmd_or_install_package_apt git git-core
   elif which rpm &> /dev/null; then
+    case $(uname -r) in
+      *el5)
+        wget http://download.fedoraproject.org/pub/epel/5/$(uname -i)/epel-release-5-4.noarch.rpm &&
+        rpm -Uvh epel-release-5-4.noarch.rpm
+        rm -f epel-release-5-4.noarch.rpm;;
+      *el6)
+        wget http://download.fedoraproject.org/pub/epel/6/$(uname -i)/epel-release-6-7.noarch.rpm &&
+        rpm -Uvh epel-release-6-7.noarch.rpm 
+        rm -f epel-release-6-7.noarch.rpm;; 
+    esac
     ensure_cmd_or_install_package_yum git git-core
   else
     abort "we only support apt-get and yum right now... please contribute!"
