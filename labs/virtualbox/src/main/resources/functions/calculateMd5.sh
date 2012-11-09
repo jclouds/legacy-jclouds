61cbd287788eff6dde30f8d1b614b1957a307225
Andrea Turli <andrea.turli@gmail.com>
2012-11-10 00:49:36 +0200

    generic md5 fixed

--- labs/virtualbox/src/main/resources/functions/calculateMd5.sh
+++ labs/virtualbox/src/main/resources/functions/calculateMd5.sh
@@ -4,17 +4,18 @@ function calculateMd5 {
       abort "calculateMd5 requires an iso file path parameter"
       return 1
    }
-   if [ which -s md5 ] 
+
+   if command -v md5 >/dev/null 2>&1
    then
-       local ISO_PATH="$0"; shift
-       local _MD5=`md5 "$ISO_PATH" | awk '{ print $4 }`
-   elif [ which -s md5sum ] 
+       local ISO_PATH="$1"; shift
+       local _MD5=`md5 "$ISO_PATH" | awk '{ print $4 }'`
+   elif command -v md5sum > /dev/null 2>&1
    then
-       local _MD5=`md5sum "$ISO_PATH" | awk '{ print $1 }`
+       local _MD5=`md5sum "$ISO_PATH" | awk '{ print $1 }'`
    fi
    [ -n "$_MD5" ] && {
    export ISO_MD5=$_MD5
-   echo [$ISO_MD5]
+   echo $ISO_MD5
     return 0
    } || {
     return 1
