2dc9533e0b358b0d7c111e1cf7d72793d4145535
Adrian Cole <adrian.cole@mulesoft.com>
2012-11-11 11:07:03 -0800

    added ability to test if an api is available based on apiVersion

---
+++ core/src/main/java/org/jclouds/rest/annotations/SinceApiVersion.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.rest.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+import org.jclouds.Constants;
+import org.jclouds.ContextBuilder;
+
+/**
+ * Designates that this resource only exists since a particular
+ * {@link ApiVersion}.
+ * 
+ * For example, in EC2, the tag api only exists at or after version
+ * {@code 2010-08-31}
+ * 
+ * @author Adrian Cole
+ * @see ApiVersion
+ */
+@Target({ TYPE, METHOD, FIELD, PARAMETER })
+@Retention(RUNTIME)
+@Qualifier
+public @interface SinceApiVersion {
+
+   /**
+    * less than or equal to the String bound to {@link ApiVersion}, typically
+    * bound as either {@link Constants#PROPERTY_API_VERSION} property or
+    * {@link ContextBuilder#apiVersion}
+    * 
+    */
+   String value();
+
+}
