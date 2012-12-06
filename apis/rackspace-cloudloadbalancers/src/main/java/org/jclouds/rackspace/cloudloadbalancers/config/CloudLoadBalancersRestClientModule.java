1f806613679b188e9827382a598f9df0b0b07fc1
Everett Toews <everett.toews@rackspace.com>
2012-12-06 10:01:03 -0600

    Renamed cloudloadbalancers to rackspace-cloudloadbalancers.

---
+++ apis/rackspace-cloudloadbalancers/src/main/java/org/jclouds/rackspace/cloudloadbalancers/config/CloudLoadBalancersRestClientModule.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.rackspace.cloudloadbalancers.config;
+
+import java.util.Map;
+
+import org.jclouds.http.HttpErrorHandler;
+import org.jclouds.http.annotation.ClientError;
+import org.jclouds.http.annotation.Redirection;
+import org.jclouds.http.annotation.ServerError;
+import org.jclouds.json.config.GsonModule.DateAdapter;
+import org.jclouds.json.config.GsonModule.Iso8601DateAdapter;
+import org.jclouds.rackspace.cloudloadbalancers.CloudLoadBalancersAsyncApi;
+import org.jclouds.rackspace.cloudloadbalancers.CloudLoadBalancersApi;
+import org.jclouds.rackspace.cloudloadbalancers.features.LoadBalancerAsyncClient;
+import org.jclouds.rackspace.cloudloadbalancers.features.LoadBalancerClient;
+import org.jclouds.rackspace.cloudloadbalancers.features.NodeAsyncClient;
+import org.jclouds.rackspace.cloudloadbalancers.features.NodeClient;
+import org.jclouds.rackspace.cloudloadbalancers.functions.ConvertLB;
+import org.jclouds.rackspace.cloudloadbalancers.handlers.ParseCloudLoadBalancersErrorFromHttpResponse;
+import org.jclouds.rest.ConfiguresRestClient;
+import org.jclouds.rest.config.RestClientModule;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+
+/**
+ * Configures theRackspace Cloud Load Balancers connection.
+ * 
+ * @author Adrian Cole
+ */
+@ConfiguresRestClient
+public class CloudLoadBalancersRestClientModule extends
+         RestClientModule<CloudLoadBalancersApi, CloudLoadBalancersAsyncApi> {
+
+   public static final Map<Class<?>, Class<?>> DELEGATE_MAP = ImmutableMap.<Class<?>, Class<?>> builder()
+            .put(LoadBalancerClient.class, LoadBalancerAsyncClient.class)
+            .put(NodeClient.class, NodeAsyncClient.class)
+            .build();
+
+   public CloudLoadBalancersRestClientModule() {
+      super(DELEGATE_MAP);
+   }
+
+   @Override
+   protected void configure() {
+      bind(DateAdapter.class).to(Iso8601DateAdapter.class);
+      install(new FactoryModuleBuilder().build(ConvertLB.Factory.class));
+      super.configure();
+   }
+
+   @Override
+   protected void bindErrorHandlers() {
+      bind(HttpErrorHandler.class).annotatedWith(Redirection.class).to(
+               ParseCloudLoadBalancersErrorFromHttpResponse.class);
+      bind(HttpErrorHandler.class).annotatedWith(ClientError.class).to(
+               ParseCloudLoadBalancersErrorFromHttpResponse.class);
+      bind(HttpErrorHandler.class).annotatedWith(ServerError.class).to(
+               ParseCloudLoadBalancersErrorFromHttpResponse.class);
+   }
+}
---
+++ apis/rackspace-cloudloadbalancers/src/main/java/org/jclouds/rackspace/cloudloadbalancers/predicates/LoadBalancerDeleted.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.rackspace.cloudloadbalancers.predicates;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import javax.annotation.Resource;
+import javax.inject.Singleton;
+
+import org.jclouds.logging.Logger;
+import org.jclouds.rackspace.cloudloadbalancers.CloudLoadBalancersApi;
+import org.jclouds.rackspace.cloudloadbalancers.domain.LoadBalancer;
+import org.jclouds.rackspace.cloudloadbalancers.domain.LoadBalancer.Status;
+
+import com.google.common.base.Predicate;
+import com.google.inject.Inject;
+
+/**
+ * 
+ * Tests to see if a loadBalancer is deleted
+ * 
+ * @author Adrian Cole
+ */
+@Singleton
+public class LoadBalancerDeleted implements Predicate<LoadBalancer> {
+
+   private final CloudLoadBalancersApi client;
+
+   @Resource
+   protected Logger logger = Logger.NULL;
+
+   @Inject
+   public LoadBalancerDeleted(CloudLoadBalancersApi client) {
+      this.client = client;
+   }
+
+   public boolean apply(LoadBalancer loadBalancer) {
+      logger.trace("looking for status on loadBalancer %s", checkNotNull(loadBalancer, "loadBalancer"));
+      loadBalancer = refresh(loadBalancer);
+      if (loadBalancer == null)
+         return true;
+      logger.trace("%s: looking for loadBalancer status %s: currently: %s", loadBalancer.getId(), Status.DELETED,
+               loadBalancer.getStatus());
+      return loadBalancer.getStatus() == Status.DELETED;
+   }
+
+   private LoadBalancer refresh(LoadBalancer loadBalancer) {
+      return client.getLoadBalancerClient(loadBalancer.getRegion()).getLoadBalancer(loadBalancer.getId());
+   }
+}
