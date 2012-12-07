b1b17ed6420f88aea15a9c262404c07debaba8d6
Ioannis Canellos <iocanel@apache.org>
2012-12-07 02:02:14 +0200

    1044: Added listener interfaces for Providers and Apis.

--- core/src/main/java/org/jclouds/osgi/MetadataBundleListener.java
+++ core/src/main/java/org/jclouds/osgi/MetadataBundleListener.java
@@ -18,26 +18,27 @@
  */
 package org.jclouds.osgi;
 
-import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicates;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
-import com.google.common.io.Closeables;
 import org.jclouds.apis.ApiMetadata;
 import org.jclouds.apis.ApiRegistry;
 import org.jclouds.providers.ProviderMetadata;
 import org.jclouds.providers.ProviderRegistry;
+import org.jclouds.util.Strings2;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
 
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Collection;
 import java.util.List;
+
+import static com.google.common.collect.Iterables.filter;
 
 /**
  * A {@link BundleListener} that listens for {@link BundleEvent} and searches for {@link org.jclouds.providers.ProviderMetadata} and {@link org.jclouds.apis.ApiMetadata} in newly
@@ -46,79 +47,105 @@ import java.util.List;
  */
 public class MetadataBundleListener implements BundleListener {
 
-   private Multimap<Long, ProviderMetadata> providerMetadataMap = ArrayListMultimap.create();
-   private Multimap<Long, ApiMetadata> apiMetadataMap = ArrayListMultimap.create();
+   private final Multimap<Long, ProviderMetadata> providerMetadataMap = ArrayListMultimap.create();
+   private final Multimap<Long, ApiMetadata> apiMetadataMap = ArrayListMultimap.create();
+
+   private final List<ProviderListener> providerListeners = Lists.newArrayList();
+   private final List<ApiListener> apiListeners = Lists.newArrayList();
 
 
-   public void start(BundleContext bundleContext) {
+   /**
+    * Starts the listener.
+    * Checks the bundles that are already active and registers {@link ProviderMetadata} and {@link ApiMetadata} found.
+    * Registers the itself as a {@link BundleListener}.
+    * @param bundleContext
+    */
+   public synchronized void start(BundleContext bundleContext) {
       bundleContext.addBundleListener(this);
       for (Bundle bundle : bundleContext.getBundles()) {
          if (bundle.getState() == Bundle.ACTIVE) {
-            List<ProviderMetadata> providerMetadataList = getProviderMetadata(bundle);
-            List<ApiMetadata> apiMetadataList = getApiMetadata(bundle);
+            addBundle(bundle);
+         }
+      }
+      bundleContext.addBundleListener(this);
+   }
 
-            for (ProviderMetadata providerMetadata : providerMetadataList) {
-               if (providerMetadata != null) {
-                  ProviderRegistry.registerProvider(providerMetadata);
-                  providerMetadataMap.put(bundle.getBundleId(), providerMetadata);
-               }
+   /**
+    * Stops the listener.
+    * Removes itself from the {@link BundleListener}s.
+    * Clears metadata maps and listeners lists.
+    * @param bundleContext
+    */
+   public void stop(BundleContext bundleContext) {
+      bundleContext.removeBundleListener(this);
+      providerMetadataMap.clear();
+      apiMetadataMap.clear();
+      apiListeners.clear();
+      providerListeners.clear();
+   }
+
+   @Override
+   public synchronized void bundleChanged(BundleEvent event) {
+      switch (event.getType()) {
+         case BundleEvent.STARTED:
+             addBundle(event.getBundle());
+            break;
+         case BundleEvent.STOPPING:
+         case BundleEvent.STOPPED:
+            removeBundle(event.getBundle());
+            break;
+      }
+   }
+
+   /**
+    * Searches for {@link ProviderMetadata} and {@link ApiMetadata} inside the {@link Bundle}.
+    * If metadata are found they are registered in the {@link ProviderRegistry} and {@link ApiRegistry}.
+    * Also the {@link ProviderListener} and {@link ApiListener} are notified.
+    * @param bundle
+    */
+   private synchronized void addBundle(Bundle bundle) {
+      for (ProviderMetadata providerMetadata : listProviderMetadata(bundle)) {
+         if (providerMetadata != null) {
+            ProviderRegistry.registerProvider(providerMetadata);
+            providerMetadataMap.put(bundle.getBundleId(), providerMetadata);
+            for (ProviderListener listener : providerListeners) {
+               listener.added(providerMetadata);
             }
+         }
+      }
 
-            for (ApiMetadata apiMetadata : apiMetadataList) {
-               if (apiMetadata != null) {
-                  ApiRegistry.registerApi(apiMetadata);
-                  apiMetadataMap.put(bundle.getBundleId(), apiMetadata);
-               }
+      for (ApiMetadata apiMetadata : listApiMetadata(bundle)) {
+         if (apiMetadata != null) {
+            ApiRegistry.registerApi(apiMetadata);
+            apiMetadataMap.put(bundle.getBundleId(), apiMetadata);
+            for (ApiListener listener : apiListeners) {
+               listener.added(apiMetadata);
             }
          }
       }
    }
 
-   public void stop(BundleContext bundleContext) {
-      providerMetadataMap.clear();
-      apiMetadataMap.clear();
-   }
-
-   @Override
-   public void bundleChanged(BundleEvent event) {
-      Collection<ProviderMetadata> providerMetadataList = null;
-      Collection<ApiMetadata> apiMetadataList = null;
-      switch (event.getType()) {
-         case BundleEvent.STARTED:
-            providerMetadataList = getProviderMetadata(event.getBundle());
-            apiMetadataList = getApiMetadata(event.getBundle());
-            for (ProviderMetadata providerMetadata : providerMetadataList) {
-               if (providerMetadata != null) {
-                  ProviderRegistry.registerProvider(providerMetadata);
-                  providerMetadataMap.put(event.getBundle().getBundleId(), providerMetadata);
-               }
-            }
-
-            for (ApiMetadata apiMetadata : apiMetadataList) {
-               if (apiMetadata != null) {
-                  ApiRegistry.registerApi(apiMetadata);
-                  apiMetadataMap.put(event.getBundle().getBundleId(), apiMetadata);
-               }
-            }
-            break;
-         case BundleEvent.STOPPING:
-         case BundleEvent.STOPPED:
-            providerMetadataList = providerMetadataMap.get(event.getBundle().getBundleId());
-            apiMetadataList = apiMetadataMap.get(event.getBundle().getBundleId());
-
-            if (providerMetadataList != null) {
-               for (ProviderMetadata providerMetadata : providerMetadataList) {
-                  ProviderRegistry.unregisterProvider(providerMetadata);
-               }
-            }
-            if (apiMetadataList != null) {
-               for (ApiMetadata apiMetadata : apiMetadataList) {
-                  ApiRegistry.unRegisterApi(apiMetadata);
-               }
-            }
-            break;
+   /**
+    * Searches for {@link ProviderMetadata} and {@link ApiMetadata} registered under the {@link Bundle} id.
+    * If metadata are found they are removed the {@link ProviderRegistry} and {@link ApiRegistry}.
+    * Also the {@link ProviderListener} and {@link ApiListener} are notified.
+    * @param bundle
+    */
+   private synchronized void removeBundle(Bundle bundle) {
+      for (ProviderMetadata providerMetadata : providerMetadataMap.removeAll(bundle.getBundleId())) {
+         ProviderRegistry.unregisterProvider(providerMetadata);
+         for (ProviderListener listener : providerListeners) {
+            listener.removed(providerMetadata);
+         }
+      }
+      for (ApiMetadata apiMetadata : apiMetadataMap.removeAll(bundle.getBundleId())) {
+         ApiRegistry.unRegisterApi(apiMetadata);
+         for (ApiListener listener : apiListeners) {
+            listener.removed(apiMetadata);
+         }
       }
    }
+
 
    /**
     * Creates an instance of {@link ProviderMetadata} from the {@link Bundle}.
@@ -126,28 +153,28 @@ public class MetadataBundleListener implements BundleListener {
     * @param bundle
     * @return
     */
-   public List<ProviderMetadata> getProviderMetadata(Bundle bundle) {
+   public Iterable<ProviderMetadata> listProviderMetadata(Bundle bundle) {
       List<ProviderMetadata> metadataList = Lists.newArrayList();
-      String classNames = getProviderMetadataClassNames(bundle);
-      if (classNames != null && !classNames.isEmpty()) {
-         for (String className : classNames.split("\n")) {
-            try {
-               Class<? extends ProviderMetadata> providerMetadataClass = bundle.loadClass(className);
-               //Classes loaded by other class loaders are not assignable.
-               if (ProviderMetadata.class.isAssignableFrom(providerMetadataClass)) {
-                  ProviderMetadata metadata = providerMetadataClass.newInstance();
-                  metadataList.add(metadata);
+      Optional<String> classNames = getProviderMetadataClassNames(bundle);
+      if (classNames.isPresent() && !classNames.get().isEmpty()) {
+            for (String className : classNames.get().split("\n")) {
+               try {
+                  Class<? extends ProviderMetadata> providerMetadataClass = bundle.loadClass(className);
+                  //Classes loaded by other class loaders are not assignable.
+                  if (ProviderMetadata.class.isAssignableFrom(providerMetadataClass)) {
+                     ProviderMetadata metadata = providerMetadataClass.newInstance();
+                     metadataList.add(metadata);
+                  }
+               } catch (ClassNotFoundException e) {
+                  // ignore
+               } catch (InstantiationException e) {
+                  // ignore
+               } catch (IllegalAccessException e) {
+                  // ignore
                }
-            } catch (ClassNotFoundException e) {
-               // ignore
-            } catch (InstantiationException e) {
-               // ignore
-            } catch (IllegalAccessException e) {
-               // ignore
             }
-         }
       }
-      return metadataList;
+      return filter(metadataList, Predicates.notNull());
    }
 
    /**
@@ -156,11 +183,11 @@ public class MetadataBundleListener implements BundleListener {
     * @param bundle
     * @return
     */
-   public List<ApiMetadata> getApiMetadata(Bundle bundle) {
+   public Iterable<ApiMetadata> listApiMetadata(Bundle bundle) {
       List<ApiMetadata> metadataList = Lists.newArrayList();
-      String classNames = getApiMetadataClassNames(bundle);
-      if (classNames != null && !classNames.isEmpty()) {
-         for (String className : classNames.split("\n")) {
+      Optional<String> classNames = getApiMetadataClassNames(bundle);
+      if (classNames.isPresent() && !classNames.get().isEmpty()) {
+         for (String className : classNames.get().split("\n")) {
             try {
                Class<? extends ApiMetadata> apiMetadataClass = bundle.loadClass(className);
                //Classes loaded by other class loaders are not assignable.
@@ -177,32 +204,85 @@ public class MetadataBundleListener implements BundleListener {
             }
          }
       }
-      return metadataList;
+      return filter(metadataList, Predicates.notNull());
    }
 
 
-   public String getMetadataClassNames(Bundle bundle, String pathToMetadata) {
-      URL resource = bundle.getEntry(pathToMetadata);
+   /**
+    * Reads the resource from a {@link Bundle}.
+    * @param bundle           The bundle to read from.
+    * @param resourcePath     The path to the resource.
+    * @return
+    */
+   public Optional<String> readResourceFromBundle(Bundle bundle, String resourcePath) {
       InputStream is = null;
-      InputStreamReader reader = null;
-      BufferedReader bufferedReader = null;
-      StringBuilder sb = new StringBuilder();
+      URL resource = bundle.getEntry(resourcePath);
+
+      if (resource == null) {
+         return Optional.absent();
+      }
 
       try {
          is = resource.openStream();
-         reader = new InputStreamReader(is, Charsets.UTF_8);
-         bufferedReader = new BufferedReader(reader);
-         String line;
-         while ((line = bufferedReader.readLine()) != null) {
-            sb.append(line).append("\n");
-         }
-      } catch (Throwable e) {
-      } finally {
-         Closeables.closeQuietly(reader);
-         Closeables.closeQuietly(bufferedReader);
-         Closeables.closeQuietly(is);
+         return Optional.of(Strings2.toStringAndClose(is));
+      } catch (IOException ex) {
+         //ignore and return absent.
+         return Optional.absent();
       }
-      return sb.toString().trim();
+   }
+
+
+   /**
+    * Adds a {@link ProviderListener} and notifies it of existing {@link ProviderMetadata}.
+    *
+    * @param listener The listener.
+    */
+   public synchronized void addProviderListener(ProviderListener listener) {
+      providerListeners.add(listener);
+      for (ProviderMetadata metadata : providerMetadataMap.values()) {
+         listener.added(metadata);
+      }
+   }
+
+   /**
+    * Removes the {@link ProviderListener}
+    *
+    * @param listener The listener
+    */
+   public synchronized void removeProviderListener(ProviderListener listener) {
+      providerListeners.remove(listener);
+   }
+
+   /**
+    * Adds a {@link ApiListener} and notifies it of existing {@link ApiMetadata}.
+    *
+    * @param listener
+    */
+   public synchronized void addApiListenerListener(ApiListener listener) {
+      apiListeners.add(listener);
+      for (ApiMetadata metadata : apiMetadataMap.values()) {
+         listener.added(metadata);
+      }
+   }
+
+   /**
+    * Removes the {@link ApiListener}
+    *
+    * @param listener
+    */
+   public synchronized void removeApiListenerListener(ApiListener listener) {
+      apiListeners.remove(listener);
+   }
+
+
+   /**
+    * Retrieves the {@link ProviderMetadata} class name for the bundle if it exists.
+    *
+    * @param bundle
+    * @return
+    */
+   public Optional<String> getProviderMetadataClassNames(Bundle bundle) {
+      return readResourceFromBundle(bundle, "/META-INF/services/org.jclouds.providers.ProviderMetadata");
    }
 
    /**
@@ -211,18 +291,7 @@ public class MetadataBundleListener implements BundleListener {
     * @param bundle
     * @return
     */
-   public String getProviderMetadataClassNames(Bundle bundle) {
-      return getMetadataClassNames(bundle, "/META-INF/services/org.jclouds.providers.ProviderMetadata");
+   public Optional<String> getApiMetadataClassNames(Bundle bundle) {
+      return readResourceFromBundle(bundle, "/META-INF/services/org.jclouds.apis.ApiMetadata");
    }
-
-   /**
-    * Retrieves the {@link ProviderMetadata} class name for the bundle if it exists.
-    *
-    * @param bundle
-    * @return
-    */
-   public String getApiMetadataClassNames(Bundle bundle) {
-      return getMetadataClassNames(bundle, "/META-INF/services/org.jclouds.apis.ApiMetadata");
-   }
-
 }
