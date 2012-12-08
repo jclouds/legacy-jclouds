3afdc3550e7c98e5a2da18b7fb6eb83a5ac84a47
Adrian Cole <adrian.cole@mulesoft.com>
2012-12-08 14:34:37 -0800

    add support for jsch proxy

--- drivers/jsch/src/main/java/org/jclouds/ssh/jsch/SessionConnection.java
+++ drivers/jsch/src/main/java/org/jclouds/ssh/jsch/SessionConnection.java
@@ -20,26 +20,35 @@ package org.jclouds.ssh.jsch;
 
 import static com.google.common.base.Objects.equal;
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
+import org.jclouds.domain.Credentials;
 import org.jclouds.domain.LoginCredentials;
+import org.jclouds.javax.annotation.Nullable;
+import org.jclouds.proxy.ProxyConfig;
 import org.jclouds.ssh.jsch.JschSshClient.Connection;
 
 import com.google.common.base.Objects;
+import com.google.common.base.Optional;
 import com.google.common.net.HostAndPort;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Proxy;
+import com.jcraft.jsch.ProxyHTTP;
+import com.jcraft.jsch.ProxySOCKS5;
 import com.jcraft.jsch.Session;
 
-public class SessionConnection implements Connection<Session> {
+public final class SessionConnection implements Connection<Session> {
    public static Builder builder() {
       return new Builder();
    }
 
-   public static class Builder {
+   public final static class Builder {
 
-      protected HostAndPort hostAndPort;
-      protected LoginCredentials loginCredentials;
-      protected int connectTimeout;
-      protected int sessionTimeout;
+      private HostAndPort hostAndPort;
+      private LoginCredentials loginCredentials;
+      private Optional<Proxy> proxy = Optional.absent();
+      private int connectTimeout;
+      private int sessionTimeout;
 
       /**
        * @see SessionConnection#getHostAndPort()
@@ -55,6 +64,39 @@ public class SessionConnection implements Connection<Session> {
       public Builder loginCredentials(LoginCredentials loginCredentials) {
          this.loginCredentials = loginCredentials;
          return this;
+      }
+
+      /**
+       * @see SessionConnection#getProxy()
+       */
+      public Builder proxy(Proxy proxy) {
+         this.proxy = Optional.fromNullable(proxy);
+         return this;
+      }
+
+      /**
+       * @see #proxy(Proxy)
+       */
+      public Builder proxy(ProxyConfig proxyConfig) {
+         Optional<HostAndPort> proxyEndpoint = proxyConfig.getProxy();
+         if (!proxyEndpoint.isPresent())
+            return proxy((Proxy) null);
+
+         Optional<Credentials> creds = proxyConfig.getCredentials();
+         switch (proxyConfig.getType()) {
+         case HTTP:
+            ProxyHTTP httpProxy = new ProxyHTTP(proxyEndpoint.get().getHostText(), proxyEndpoint.get().getPort());
+            if (creds.isPresent())
+               httpProxy.setUserPasswd(creds.get().identity, creds.get().credential);
+            return proxy(httpProxy);
+         case SOCKS:
+            ProxySOCKS5 socksProxy = new ProxySOCKS5(proxyEndpoint.get().getHostText(), proxyEndpoint.get().getPort());
+            if (creds.isPresent())
+               socksProxy.setUserPasswd(creds.get().identity, creds.get().credential);
+            return proxy(socksProxy);
+         default:
+            throw new IllegalArgumentException(proxyConfig.getType() + " not supported");
+         }
       }
 
       /**
@@ -74,27 +116,30 @@ public class SessionConnection implements Connection<Session> {
       }
 
       public SessionConnection build() {
-         return new SessionConnection(hostAndPort, loginCredentials, connectTimeout, sessionTimeout);
+         return new SessionConnection(hostAndPort, loginCredentials, proxy, connectTimeout, sessionTimeout);
       }
 
-      protected Builder fromSessionConnection(SessionConnection in) {
-         return hostAndPort(in.getHostAndPort()).connectTimeout(in.getConnectTimeout()).loginCredentials(
-                  in.getLoginCredentials());
+      public Builder from(SessionConnection in) {
+         return hostAndPort(in.hostAndPort).loginCredentials(in.loginCredentials).proxy(in.proxy.orNull())
+               .connectTimeout(in.connectTimeout).sessionTimeout(in.sessionTimeout);
       }
+
    }
 
-   private SessionConnection(HostAndPort hostAndPort, LoginCredentials loginCredentials, int connectTimeout,
-            int sessionTimeout) {
-      this.hostAndPort = hostAndPort;
-      this.loginCredentials = loginCredentials;
+   private SessionConnection(HostAndPort hostAndPort, LoginCredentials loginCredentials, Optional<Proxy> proxy,
+         int connectTimeout, int sessionTimeout) {
+      this.hostAndPort = checkNotNull(hostAndPort, "hostAndPort");
+      this.loginCredentials = checkNotNull(loginCredentials, "loginCredentials for %", hostAndPort);
       this.connectTimeout = connectTimeout;
       this.sessionTimeout = sessionTimeout;
+      this.proxy = checkNotNull(proxy, "proxy for %", hostAndPort);
    }
 
    private static final byte[] emptyPassPhrase = new byte[0];
 
    private final HostAndPort hostAndPort;
    private final LoginCredentials loginCredentials;
+   private final Optional<Proxy> proxy;
    private final int connectTimeout;
    private final int sessionTimeout;
 
@@ -112,7 +157,7 @@ public class SessionConnection implements Connection<Session> {
    public Session create() throws Exception {
       JSch jsch = new JSch();
       session = jsch
-               .getSession(loginCredentials.getUser(), hostAndPort.getHostText(), hostAndPort.getPortOrDefault(22));
+            .getSession(loginCredentials.getUser(), hostAndPort.getHostText(), hostAndPort.getPortOrDefault(22));
       if (sessionTimeout != 0)
          session.setTimeout(sessionTimeout);
       if (loginCredentials.getPrivateKey() == null) {
@@ -126,6 +171,8 @@ public class SessionConnection implements Connection<Session> {
       java.util.Properties config = new java.util.Properties();
       config.put("StrictHostKeyChecking", "no");
       session.setConfig(config);
+      if (proxy.isPresent())
+         session.setProxy(proxy.get());
       session.connect(connectTimeout);
       return session;
    }
@@ -143,6 +190,14 @@ public class SessionConnection implements Connection<Session> {
     */
    public LoginCredentials getLoginCredentials() {
       return loginCredentials;
+   }
+
+   /**
+    * 
+    * @return proxy used for this connection
+    */
+   public Optional<Proxy> getProxy() {
+      return proxy;
    }
 
    /**
@@ -165,6 +220,7 @@ public class SessionConnection implements Connection<Session> {
     * 
     * @return the current session or {@code null} if not connected
     */
+   @Nullable
    public Session getSession() {
       return session;
    }
@@ -177,7 +233,7 @@ public class SessionConnection implements Connection<Session> {
          return false;
       SessionConnection that = SessionConnection.class.cast(o);
       return equal(this.hostAndPort, that.hostAndPort) && equal(this.loginCredentials, that.loginCredentials)
-               && equal(this.session, that.session);
+            && equal(this.session, that.session);
    }
 
    @Override
@@ -187,10 +243,12 @@ public class SessionConnection implements Connection<Session> {
 
    @Override
    public String toString() {
-      return Objects.toStringHelper("").add("hostAndPort", hostAndPort).add("loginUser", loginCredentials.getUser())
-               .add("session", session != null ? session.hashCode() : null).add("connectTimeout", connectTimeout).add(
-                        "sessionTimeout", sessionTimeout).toString();
+      return Objects.toStringHelper("").omitNullValues()
+            .add("hostAndPort", hostAndPort).add("loginUser", loginCredentials.getUser())
+            .add("session", session != null ? session.hashCode() : null)
+            .add("connectTimeout", connectTimeout)
+            .add("proxy", proxy.orNull())
+            .add("sessionTimeout", sessionTimeout).toString();
    }
-
 
 }
