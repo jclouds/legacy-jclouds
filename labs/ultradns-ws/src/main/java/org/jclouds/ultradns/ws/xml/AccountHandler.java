8d1e797edb184939e746a2047c2fb00ab92dabf8
adriancole <adrian.f.cole@gmail.com>
2013-01-26 12:53:55 -0800

    issue #1165: added skeleton for UltraDNS WS provider

---
+++ labs/ultradns-ws/src/main/java/org/jclouds/ultradns/ws/xml/AccountHandler.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.ultradns.ws.xml;
+
+import static org.jclouds.util.SaxUtils.cleanseAttributes;
+import static org.jclouds.util.SaxUtils.equalsOrSuffix;
+
+import java.util.Map;
+
+import org.jclouds.http.functions.ParseSax;
+import org.jclouds.ultradns.ws.domain.Account;
+import org.xml.sax.Attributes;
+
+/**
+ * 
+ * @author Adrian Cole
+ */
+public class AccountHandler extends ParseSax.HandlerForGeneratedRequestWithResult<Account> {
+
+   private Account account;
+
+   @Override
+   public Account getResult() {
+      try {
+         return account;
+      } finally {
+         account = null;
+      }
+   }
+
+   @Override
+   public void startElement(String uri, String localName, String qName, Attributes attrs) {
+      Map<String, String> attributes = cleanseAttributes(attrs);
+      if (equalsOrSuffix(qName, "AccountDetailsData")) {
+         account = Account.fromIdAndName(attributes.get("accountID"), attributes.get("accountName"));
+      }
+   }
+}
---
+++ labs/ultradns-ws/src/test/java/org/jclouds/ultradns/ws/UltraDNSWSApiExpectTest.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unles required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either expres or implied.  See the License for the
+ * specific language governing permisions and limitations
+ * under the License.
+ */
+package org.jclouds.ultradns.ws;
+
+import static org.testng.Assert.assertEquals;
+
+import org.jclouds.http.HttpRequest;
+import org.jclouds.http.HttpResponse;
+import org.jclouds.ultradns.ws.internal.BaseUltraDNSWSApiExpectTest;
+import org.jclouds.ultradns.ws.parse.GetAccountsListOfUserResponseTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author Adrian Cole
+ */
+@Test(groups = "unit", testName = "UltraDNSWSApiExpectTest")
+public class UltraDNSWSApiExpectTest extends BaseUltraDNSWSApiExpectTest {
+
+   HttpRequest getCurrentAccount = HttpRequest.builder().method("POST")
+         .endpoint("http://ultra-api.ultradns.com:8008/UltraDNS_WS/v01/")
+         .addHeader("Host", "ultra-api.ultradns.com:8008")
+         .payload(payloadFromResourceWithContentType("/get_current_account.xml", "application/xml")).build();
+
+   HttpResponse getCurrentAccountResponse = HttpResponse.builder().statusCode(200)
+         .payload(payloadFromResourceWithContentType("/account.xml", "application/xml")).build();
+
+   public void testGetCurrentAccountWhenResponseIs2xx() {
+
+      UltraDNSWSApi apiWhenWithOptionsExist = requestSendsResponse(getCurrentAccount, getCurrentAccountResponse);
+
+      assertEquals(
+            apiWhenWithOptionsExist.getCurrentAccount().toString(),
+            new GetAccountsListOfUserResponseTest().expected().toString());
+   }
+}
