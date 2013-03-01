f9513bec8467ce8f7df5b292e9da0ceff83e75d8
ilja.bobkevic <ilja.bobkevic@gmail.com>
2013-03-01 22:23:27 +0100

    Add flavor create and delete API calls for openstack-nova
    1. Harden Flavor
    2. Update FlavorApi and FlavorAsyncApi by adding create(...) and
    delete(...) methods
    3. Add flavor_new.json and ParseCreateFlavorTest
    4. Add create and delete tests into FlavorApiExpectTest
    
    Change-Id: I3ea15883fb655d9a1dc48fb4ed1b1613d4975d93

---
+++ apis/openstack-nova/src/test/java/org/jclouds/openstack/nova/v2_0/parse/ParseCreateFlavorTest.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.openstack.nova.v2_0.parse;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+import org.jclouds.json.BaseItemParserTest;
+import org.jclouds.json.config.GsonModule;
+import org.jclouds.openstack.nova.v2_0.config.NovaParserModule;
+import org.jclouds.openstack.nova.v2_0.domain.Flavor;
+import org.jclouds.openstack.nova.v2_0.features.FlavorApiExpectTest;
+import org.jclouds.rest.annotations.SelectJson;
+import org.testng.annotations.Test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+/**
+ * @see FlavorApiExpectTest
+ * @author Ilja Bobkevic
+ */
+@Test(groups = "unit", testName = "ParseCreateFlavorTest")
+public class ParseCreateFlavorTest extends BaseItemParserTest<Flavor> {
+
+	@Override
+	public String resource() {
+		return "/flavor_new.json";
+	}
+
+	@Override
+	@SelectJson("flavor")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Flavor expected() {
+		return Flavor.builder()
+				.id("1cb47a44-9b84-4da4-bf81-c1976e8414ab")
+				.name("128 MB Server").ram(128).vcpus(1)
+				.disk(10).build();
+	}
+
+	@Override
+	protected Injector injector() {
+		return Guice.createInjector(new NovaParserModule(), new GsonModule());
+	}
+}
