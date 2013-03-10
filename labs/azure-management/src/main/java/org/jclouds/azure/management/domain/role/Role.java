f4cfeb3b7094e22b35c065ac38da469208b40f08
Adrian Cole <adrian.f.cole@gmail.com>
2013-03-10 09:35:06 -0700

    issue #1401: labs is now at https://github.com/jclouds/jclouds-labs

--- labs/azure-management/src/main/java/org/jclouds/azure/management/domain/role/Role.java
+++
@@ -1,8 +0,0 @@
-package org.jclouds.azure.management.domain.role;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "Role")
-public class Role extends AbstractRole {
-
-}
--- labs/virtualbox/src/test/resources/test_guest_additions_installer.sh
+++
@@ -1,6 +0,0 @@
-setupPublicCurl || return 1
-(mkdir -p /tmp/ && cd /tmp/ && [ ! -f VBoxGuestAdditions_4.1.6.iso ] && curl -q -s -S -L --connect-timeout 10 --max-time 600 --retry 20 -C - -X GET  http://download.virtualbox.org/virtualbox/4.1.6/VBoxGuestAdditions_4.1.6.iso >VBoxGuestAdditions_4.1.6.iso)
-mount -o loop /tmp/VBoxGuestAdditions_4.1.6.iso /mnt
-installModuleAssistantIfNeeded || return 1
-/mnt/VBoxLinuxAdditions.run
-umount /mnt
