0fab297e0c13578e0394238d2004dd2bf58fe1af
Adrian Cole <adrian.f.cole@gmail.com>
2013-03-10 09:35:06 -0700

    issue #1401: labs is now at https://github.com/jclouds/jclouds-labs

--- labs/azure-management/src/main/java/org/jclouds/azure/management/domain/role/PersistentVMRole.java
+++
@@ -1,8 +0,0 @@
-package org.jclouds.azure.management.domain.role;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "PersistentVMRole")
-public class PersistentVMRole extends AbstractRole {
-
-}
