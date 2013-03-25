72d910dd4cd8d8ae3748ee38157bc0573fa1e8a7
Everett Toews <everett.toews@rackspace.com>
2013-03-25 12:51:17 -0500

    PaginatedCollection for Rackspace style pagination.

---
+++ apis/rackspace-cloudidentity/src/main/java/org/jclouds/rackspace/cloudidentity/v2_0/functions/DateParser.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.rackspace.cloudidentity.v2_0.functions;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Date;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.jclouds.date.DateService;
+
+import com.google.common.base.Function;
+
+/**
+ * Takes a Date and return a yyyy-MM-dd String.
+ * 
+ * @author Everett Toews
+ */
+@Singleton
+public class DateParser implements Function<Object, String> {
+   
+   private final DateService dateService;
+   
+   @Inject
+   DateParser(DateService dateService) {
+      this.dateService = dateService;
+   }
+   
+   @Override
+   public String apply(Object input) {
+      checkArgument(checkNotNull(input, "input") instanceof Date, "This function is only valid for Dates!");
+      Date date = Date.class.cast(input);
+
+      return dateService.iso8601SecondsDateFormat(date);
+   }
+
+}
---
+++ apis/rackspace-cloudidentity/src/test/java/org/jclouds/rackspace/cloudidentity/v2_0/CloudIdentityFallbacks.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.rackspace.cloudidentity.v2_0;
+
+import static com.google.common.util.concurrent.Futures.immediateFuture;
+import static org.jclouds.Fallbacks.valOnNotFoundOr404;
+
+import org.jclouds.Fallback;
+import org.jclouds.openstack.v2_0.domain.Link;
+import org.jclouds.rackspace.cloudidentity.v2_0.domain.PaginatedCollection;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.ListenableFuture;
+
+public final class CloudIdentityFallbacks {
+   private CloudIdentityFallbacks() {
+   }
+
+   public static final class EmptyPaginatedCollectionOnNotFoundOr404 implements Fallback<PaginatedCollection<Object>> {
+      private static final PaginatedCollection<Object> EMPTY = new PaginatedCollection<Object>(
+            ImmutableSet.<Object> of(), ImmutableSet.<Link> of(), 0) {
+      };
+
+      @Override
+      public ListenableFuture<PaginatedCollection<Object>> create(Throwable t) throws Exception {
+         return immediateFuture(createOrPropagate(t));
+      }
+
+      @Override
+      public PaginatedCollection<Object> createOrPropagate(Throwable t) throws Exception {
+         return valOnNotFoundOr404(EMPTY, t);
+      }
+   }
+}
