413538c45326e0fd323e896b1f93c7693eeec44f
adriancole <adrian.f.cole@gmail.com>
2013-04-08 18:03:37 -0700

    refactored aws-s3 MPU test

--- providers/aws-s3/src/main/java/org/jclouds/aws/s3/blobstore/strategy/internal/SequentialMultipartUploadStrategy.java
+++ providers/aws-s3/src/main/java/org/jclouds/aws/s3/blobstore/strategy/internal/SequentialMultipartUploadStrategy.java
@@ -25,19 +25,16 @@ import java.util.SortedMap;
 import javax.annotation.Resource;
 import javax.inject.Named;
 
-import org.jclouds.aws.s3.AWSS3ApiMetadata;
 import org.jclouds.aws.s3.AWSS3Client;
-import org.jclouds.aws.s3.blobstore.AWSS3BlobStore;
 import org.jclouds.aws.s3.blobstore.strategy.MultipartUploadStrategy;
 import org.jclouds.blobstore.KeyNotFoundException;
 import org.jclouds.blobstore.domain.Blob;
-import org.jclouds.blobstore.options.PutOptions;
 import org.jclouds.blobstore.reference.BlobStoreConstants;
 import org.jclouds.io.Payload;
 import org.jclouds.io.PayloadSlicer;
 import org.jclouds.logging.Logger;
+import org.jclouds.s3.blobstore.functions.BlobToObject;
 import org.jclouds.s3.domain.ObjectMetadataBuilder;
-import org.jclouds.util.Throwables2;
 
 import com.google.common.collect.Maps;
 import com.google.inject.Inject;
@@ -47,86 +44,83 @@ import com.google.inject.Inject;
  * 
  * The file partitioning algorithm:
  * 
- * The default partition size we choose is 32mb. A multiple of this default partition size is used.
- * The number of parts first grows to a chosen magnitude (for example 100 parts), then it grows the
- * partition size instead of number of partitions. When we reached the maximum part size, then again
- * it starts to grow the number of partitions.
+ * The default partition size we choose is 32mb. A multiple of this default
+ * partition size is used. The number of partCount first grows to a chosen magnitude
+ * (for example 100 partCount), then it grows the partition size instead of number
+ * of partitions. When we reached the maximum part size, then again it starts to
+ * grow the number of partitions.
  * 
- * @author Tibor Kiss
+ * @author Tibor Kiss, Adrian Cole
  */
 public class SequentialMultipartUploadStrategy implements MultipartUploadStrategy {
    @Resource
    @Named(BlobStoreConstants.BLOBSTORE_LOGGER)
-   protected Logger logger = Logger.NULL;
+   private Logger logger = Logger.NULL;
 
-   protected final AWSS3BlobStore ablobstore;
-   protected final PayloadSlicer slicer;
+   private final AWSS3Client client;
+   private final BlobToObject blobToObject;
+   private final MultipartUploadSlicingAlgorithm algorithm;
+   private final PayloadSlicer slicer;
 
    @Inject
-   public SequentialMultipartUploadStrategy(AWSS3BlobStore ablobstore, PayloadSlicer slicer) {
-      this.ablobstore = checkNotNull(ablobstore, "ablobstore");
+   public SequentialMultipartUploadStrategy(AWSS3Client client, BlobToObject blobToObject,
+         MultipartUploadSlicingAlgorithm algorithm, PayloadSlicer slicer) {
+      this.client = checkNotNull(client, "client");
+      this.blobToObject = checkNotNull(blobToObject, "blobToObject");
+      this.algorithm = checkNotNull(algorithm, "algorithm");
       this.slicer = checkNotNull(slicer, "slicer");
    }
-   
-   protected void prepareUploadPart(String container, String key, String uploadId, int part,
-         Payload payload, long offset, long size, SortedMap<Integer, String> etags) {
-      AWSS3Client client = ablobstore.getContext().unwrap(AWSS3ApiMetadata.CONTEXT_TOKEN).getApi();
+
+   @Override
+   public String execute(String container, Blob blob) {
+      String key = blob.getMetadata().getName();
+      Payload payload = blob.getPayload();
+      Long length = payload.getContentMetadata().getContentLength();
+      checkNotNull(length,
+            "please invoke payload.getContentMetadata().setContentLength(length) prior to multipart upload");
+      long chunkSize = algorithm.calculateChunkSize(length);
+      int partCount = algorithm.getParts();
+      if (partCount > 0) {
+         String uploadId = client.initiateMultipartUpload(container, ObjectMetadataBuilder.create().key(key).build());
+         try {
+            SortedMap<Integer, String> etags = Maps.newTreeMap();
+            int part;
+            while ((part = algorithm.getNextPart()) <= partCount) {
+               prepareUploadPart(container, key, uploadId, part, payload, algorithm.getNextChunkOffset(), chunkSize,
+                     etags);
+            }
+            long remaining = algorithm.getRemaining();
+            if (remaining > 0) {
+               prepareUploadPart(container, key, uploadId, part, payload, algorithm.getNextChunkOffset(), remaining,
+                     etags);
+            }
+            return client.completeMultipartUpload(container, key, uploadId, etags);
+         } catch (RuntimeException ex) {
+            client.abortMultipartUpload(container, key, uploadId);
+            throw ex;
+         }
+      } else {
+         // TODO: find a way to disable multipart. if we pass the original
+         // options, it goes into a stack overflow
+         return client.putObject(container, blobToObject.apply(blob));
+      }
+   }
+
+   private void prepareUploadPart(String container, String key, String uploadId, int part, Payload payload,
+         long offset, long size, SortedMap<Integer, String> etags) {
       Payload chunkedPart = slicer.slice(payload, offset, size);
       String eTag = null;
       try {
          eTag = client.uploadPart(container, key, part, uploadId, chunkedPart);
          etags.put(Integer.valueOf(part), eTag);
       } catch (KeyNotFoundException e) {
-         // note that because of eventual consistency, the upload id may not be present yet
-         // we may wish to add this condition to the retry handler
+         // note that because of eventual consistency, the upload id may not be
+         // present yet we may wish to add this condition to the retry handler
 
-         // we may also choose to implement ListParts and wait for the uploadId to become
-         // available there.
+         // we may also choose to implement ListParts and wait for the uploadId
+         // to become available there.
          eTag = client.uploadPart(container, key, part, uploadId, chunkedPart);
          etags.put(Integer.valueOf(part), eTag);
-      }
-   }
-
-   @Override
-   public String execute(String container, Blob blob, PutOptions options) {
-      String key = blob.getMetadata().getName();
-      Payload payload = blob.getPayload();
-      MultipartUploadSlicingAlgorithm algorithm = new MultipartUploadSlicingAlgorithm();
-      algorithm
-               .calculateChunkSize(checkNotNull(
-                        payload.getContentMetadata().getContentLength(),
-                        "contentLength required on all uploads to amazon s3; please invoke payload.getContentMetadata().setContentLength(length) first"));
-      int parts = algorithm.getParts();
-      long chunkSize = algorithm.getChunkSize();
-      if (parts > 0) {
-         AWSS3Client client = ablobstore.getContext()
-               .unwrap(AWSS3ApiMetadata.CONTEXT_TOKEN).getApi();
-         String uploadId = client.initiateMultipartUpload(container,
-               ObjectMetadataBuilder.create().key(key).build()); // TODO md5
-         try {
-            SortedMap<Integer, String> etags = Maps.newTreeMap();
-            int part;
-            while ((part = algorithm.getNextPart()) <= parts) {
-               prepareUploadPart(container, key, uploadId, part, payload, 
-                     algorithm.getNextChunkOffset(), chunkSize, etags);
-            }
-            long remaining = algorithm.getRemaining();
-            if (remaining > 0) {
-               prepareUploadPart(container, key, uploadId, part, payload, 
-                     algorithm.getNextChunkOffset(), remaining, etags);
-            }
-            return client.completeMultipartUpload(container, key, uploadId, etags);
-         } catch (Exception ex) {
-            RuntimeException rtex = Throwables2.getFirstThrowableOfType(ex, RuntimeException.class);
-            if (rtex == null) {
-               rtex = new RuntimeException(ex);
-            }
-            client.abortMultipartUpload(container, key, uploadId);
-            throw rtex;
-         }
-      } else {
-         // TODO: find a way to disable multipart.  if we pass the original options, it goes into a stack overflow
-         return ablobstore.putBlob(container, blob, PutOptions.NONE);
       }
    }
 }
