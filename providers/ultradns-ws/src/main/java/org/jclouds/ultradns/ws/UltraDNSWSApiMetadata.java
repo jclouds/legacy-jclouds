4a41e245f4b4873396c7d930c30f4d62ffa63641
adriancole <adrian.f.cole@gmail.com>
2013-04-09 22:59:29 -0700

    removed async api from ultradns-ws

--- providers/ultradns-ws/src/main/java/org/jclouds/ultradns/ws/UltraDNSWSApiMetadata.java
+++ providers/ultradns-ws/src/main/java/org/jclouds/ultradns/ws/UltraDNSWSApiMetadata.java
@@ -22,38 +22,37 @@ import java.net.URI;
 import java.util.Properties;
 
 import org.jclouds.apis.ApiMetadata;
-import org.jclouds.rest.internal.BaseRestApiMetadata;
-import org.jclouds.ultradns.ws.config.UltraDNSWSRestClientModule;
+import org.jclouds.rest.internal.BaseHttpApiMetadata;
+import org.jclouds.ultradns.ws.config.UltraDNSWSHttpApiModule;
 
 /**
  * Implementation of {@link ApiMetadata} for Neustar's UltraDNSWS api.
  * 
  * @author Adrian Cole
  */
-public class UltraDNSWSApiMetadata extends BaseRestApiMetadata {
+public class UltraDNSWSApiMetadata extends BaseHttpApiMetadata<UltraDNSWSApi> {
 
    @Override
    public Builder toBuilder() {
-      return new Builder(getApi(), getAsyncApi()).fromApiMetadata(this);
+      return new Builder().fromApiMetadata(this);
    }
 
    public UltraDNSWSApiMetadata() {
-      this(new Builder(UltraDNSWSApi.class, UltraDNSWSAsyncApi.class));
+      this(new Builder());
    }
 
    protected UltraDNSWSApiMetadata(Builder builder) {
-      super(Builder.class.cast(builder));
+      super(builder);
    }
 
    public static Properties defaultProperties() {
-      Properties properties = BaseRestApiMetadata.defaultProperties();
+      Properties properties = BaseHttpApiMetadata.defaultProperties();
       return properties;
    }
 
-   public static class Builder extends BaseRestApiMetadata.Builder<Builder> {
+   public static class Builder extends BaseHttpApiMetadata.Builder<UltraDNSWSApi, Builder> {
 
-      protected Builder(Class<?> api, Class<?> asyncApi) {
-         super(api, asyncApi);
+      protected Builder() {
          id("ultradns-ws")
          .name("Neustar UltraDNS WS Api")
          .identityName("Username")
@@ -62,7 +61,7 @@ public class UltraDNSWSApiMetadata extends BaseRestApiMetadata {
          .documentation(URI.create("https://portal.ultradns.com/static/docs/NUS_API_XML_SOAP.pdf"))
          .defaultEndpoint("https://ultra-api.ultradns.com:8443/UltraDNS_WS/v01")
          .defaultProperties(UltraDNSWSApiMetadata.defaultProperties())
-         .defaultModule(UltraDNSWSRestClientModule.class);
+         .defaultModule(UltraDNSWSHttpApiModule.class);
       }
 
       @Override
--- providers/ultradns-ws/src/main/java/org/jclouds/ultradns/ws/features/TrafficControllerPoolApi.java
+++ providers/ultradns-ws/src/main/java/org/jclouds/ultradns/ws/features/TrafficControllerPoolApi.java
@@ -18,29 +18,43 @@
  */
 package org.jclouds.ultradns.ws.features;
 
+import javax.inject.Named;
+import javax.ws.rs.POST;
+
+import org.jclouds.Fallbacks.NullOnNotFoundOr404;
+import org.jclouds.Fallbacks.VoidOnNotFoundOr404;
 import org.jclouds.javax.annotation.Nullable;
 import org.jclouds.rest.ResourceNotFoundException;
+import org.jclouds.rest.annotations.Fallback;
+import org.jclouds.rest.annotations.MapBinder;
+import org.jclouds.rest.annotations.Payload;
+import org.jclouds.rest.annotations.PayloadParam;
+import org.jclouds.rest.annotations.RequestFilters;
+import org.jclouds.rest.annotations.VirtualHost;
+import org.jclouds.rest.annotations.XMLResponseParser;
 import org.jclouds.ultradns.ws.UltraDNSWSExceptions.ResourceAlreadyExistsException;
+import org.jclouds.ultradns.ws.binders.UpdatePoolRecordToXML;
 import org.jclouds.ultradns.ws.domain.PoolRecordSpec;
 import org.jclouds.ultradns.ws.domain.TrafficControllerPool;
 import org.jclouds.ultradns.ws.domain.TrafficControllerPoolRecord;
 import org.jclouds.ultradns.ws.domain.UpdatePoolRecord;
+import org.jclouds.ultradns.ws.filters.SOAPWrapWithPasswordAuth;
+import org.jclouds.ultradns.ws.xml.AttributeHandler;
+import org.jclouds.ultradns.ws.xml.ElementTextHandler;
+import org.jclouds.ultradns.ws.xml.PoolRecordSpecHandler;
+import org.jclouds.ultradns.ws.xml.TrafficControllerPoolListHandler;
+import org.jclouds.ultradns.ws.xml.TrafficControllerPoolRecordListHandler;
 
 import com.google.common.collect.FluentIterable;
 
 /**
- * @see TrafficControllerPoolAsyncApi
+ * @see <a href="https://ultra-api.ultradns.com:8443/UltraDNS_WS/v01?wsdl" />
+ * @see <a href="https://www.ultradns.net/api/NUS_API_XML_SOAP.pdf" />
  * @author Adrian Cole
  */
+@RequestFilters(SOAPWrapWithPasswordAuth.class)
+@VirtualHost
 public interface TrafficControllerPoolApi {
-
-   /**
-    * Returns all traffic controller pools in the zone.
-    * 
-    * @throws ResourceNotFoundException
-    *            if the zone doesn't exist
-    */
-   FluentIterable<TrafficControllerPool> list() throws ResourceNotFoundException;
 
    /**
     * creates a traffic controller pool.
@@ -54,7 +68,37 @@ public interface TrafficControllerPoolApi {
     * @throws ResourceAlreadyExistsException
     *            if a pool already exists with the same attrs
     */
-   String createPoolForHostname(String name, String hostname) throws ResourceAlreadyExistsException;
+   @Named("addTCLBPool")
+   @POST
+   @XMLResponseParser(ElementTextHandler.TCPoolID.class)
+   @Payload("<v01:addTCLBPool><transactionID /><zoneName>{zoneName}</zoneName><hostName>{hostName}</hostName><description>{description}</description><poolRecordType>1</poolRecordType><failOver>Enabled</failOver><probing>Enabled</probing><maxActive>0</maxActive><rrGUID /></v01:addTCLBPool>")
+   String createPoolForHostname(@PayloadParam("description") String name, @PayloadParam("hostName") String hostname)
+         throws ResourceAlreadyExistsException;
+
+   /**
+    * Returns all traffic controller pools in the zone.
+    * 
+    * @throws ResourceNotFoundException
+    *            if the zone doesn't exist
+    */
+   @Named("getLoadBalancingPoolsByZone")
+   @POST
+   @XMLResponseParser(TrafficControllerPoolListHandler.class)
+   @Payload("<v01:getLoadBalancingPoolsByZone><zoneName>{zoneName}</zoneName><lbPoolType>TC</lbPoolType></v01:getLoadBalancingPoolsByZone>")
+   FluentIterable<TrafficControllerPool> list() throws ResourceNotFoundException;
+
+   /**
+    * Returns all records in the traffic controller pool.
+    * 
+    * @throws ResourceNotFoundException
+    *            if the pool doesn't exist
+    */
+   @Named("getPoolRecords")
+   @POST
+   @XMLResponseParser(TrafficControllerPoolRecordListHandler.class)
+   @Payload("<v01:getPoolRecords><poolId>{poolId}</poolId></v01:getPoolRecords>")
+   FluentIterable<TrafficControllerPoolRecord> listRecords(@PayloadParam("poolId") String poolId)
+         throws ResourceNotFoundException;
 
    /**
     * Retrieves the name of the specified pool by dname.
@@ -64,7 +108,12 @@ public interface TrafficControllerPoolApi {
     * @return null if not found
     */
    @Nullable
-   String getNameByDName(String dname);
+   @Named("getPoolForPoolHostName>")
+   @POST
+   @Payload("<v01:getPoolForPoolHostName><hostName>{hostName}</hostName></v01:getPoolForPoolHostName>")
+   @XMLResponseParser(AttributeHandler.PoolName.class)
+   @Fallback(NullOnNotFoundOr404.class)
+   String getNameByDName(@PayloadParam("hostName") String dname);
 
    /**
     * removes a pool and all its records and probes
@@ -72,15 +121,11 @@ public interface TrafficControllerPoolApi {
     * @param id
     *           the {@link TrafficControllerPool#getId() id}
     */
-   void delete(String id);
-
-   /**
-    * Returns all records in the traffic controller pool.
-    * 
-    * @throws ResourceNotFoundException
-    *            if the pool doesn't exist
-    */
-   FluentIterable<TrafficControllerPoolRecord> listRecords(String poolId) throws ResourceNotFoundException;
+   @Named("deleteLBPool")
+   @POST
+   @Payload("<v01:deleteLBPool><transactionID /><lbPoolID>{lbPoolID}</lbPoolID><DeleteAll>Yes</DeleteAll><retainRecordId /></v01:deleteLBPool>")
+   @Fallback(VoidOnNotFoundOr404.class)
+   void delete(@PayloadParam("lbPoolID") String id);
 
    /**
     * adds a new record to the pool with default weight.
@@ -96,7 +141,12 @@ public interface TrafficControllerPoolApi {
     * @throws ResourceAlreadyExistsException
     *            if a record already exists with the same attrs
     */
-   String addRecordToPoolWithTTL(String pointsTo, String lbPoolID, int ttl) throws ResourceAlreadyExistsException;
+   @Named("addPoolRecord")
+   @POST
+   @XMLResponseParser(ElementTextHandler.PoolRecordID.class)
+   @Payload("<v01:addPoolRecord><transactionID /><poolID>{poolID}</poolID><pointsTo>{pointsTo}</pointsTo><priority /><failOverDelay /><ttl>{ttl}</ttl><weight /><mode /><threshold /></v01:addPoolRecord>")
+   String addRecordToPoolWithTTL(@PayloadParam("pointsTo") String pointsTo, @PayloadParam("poolID") String lbPoolID,
+         @PayloadParam("ttl") int ttl) throws ResourceAlreadyExistsException;
 
    /**
     * adds a new record to the pool with a specified weight.
@@ -114,7 +164,12 @@ public interface TrafficControllerPoolApi {
     * @throws ResourceAlreadyExistsException
     *            if a record already exists with the same attrs
     */
-   String addRecordToPoolWithTTLAndWeight(String pointsTo, String lbPoolID, int ttl, int weight)
+   @Named("addPoolRecord")
+   @POST
+   @XMLResponseParser(ElementTextHandler.PoolRecordID.class)
+   @Payload("<v01:addPoolRecord><transactionID /><poolID>{poolID}</poolID><pointsTo>{pointsTo}</pointsTo><priority /><failOverDelay /><ttl>{ttl}</ttl><weight>{weight}</weight><mode /><threshold /></v01:addPoolRecord>")
+   String addRecordToPoolWithTTLAndWeight(@PayloadParam("pointsTo") String pointsTo,
+         @PayloadParam("poolID") String lbPoolID, @PayloadParam("ttl") int ttl, @PayloadParam("weight") int weight)
          throws ResourceAlreadyExistsException;
 
    /**
@@ -122,8 +177,13 @@ public interface TrafficControllerPoolApi {
     *           {@link TrafficControllerPoolRecord#getId()}
     * @return null if not found
     */
+   @Named("getPoolRecordSpec>")
+   @POST
+   @Payload("<v01:getPoolRecordSpec><poolRecordId>{poolRecordId}</poolRecordId></v01:getPoolRecordSpec>")
+   @XMLResponseParser(PoolRecordSpecHandler.class)
+   @Fallback(NullOnNotFoundOr404.class)
    @Nullable
-   PoolRecordSpec getRecordSpec(String poolRecordID);
+   PoolRecordSpec getRecordSpec(@PayloadParam("poolRecordId") String poolRecordID);
 
    /**
     * This request updates an existing pool record.
@@ -137,7 +197,11 @@ public interface TrafficControllerPoolApi {
     * @throws ResourceNotFoundException
     *            if the record doesn't exist
     */
-   void updateRecord(String poolRecordID, UpdatePoolRecord update) throws ResourceNotFoundException;
+   @Named("updatePoolRecord>")
+   @POST
+   @MapBinder(UpdatePoolRecordToXML.class)
+   void updateRecord(@PayloadParam("poolRecordID") String poolRecordID, @PayloadParam("update") UpdatePoolRecord update)
+         throws ResourceNotFoundException;
 
    /**
     * deletes a specific pooled resource record
@@ -145,6 +209,10 @@ public interface TrafficControllerPoolApi {
     * @param poolRecordID
     *           {@see TrafficControllerPoolRecord#getId()}
     */
-   void deleteRecord(String poolRecordID);
+   @Named("deletePoolRecord")
+   @POST
+   @Payload("<v01:deletePoolRecord><transactionID /><poolRecordID>{poolRecordID}</poolRecordID><parentPoolId /><childPoolId /></v01:deletePoolRecord>")
+   @Fallback(VoidOnNotFoundOr404.class)
+   void deleteRecord(@PayloadParam("poolRecordID") String poolRecordID);
 
 }
