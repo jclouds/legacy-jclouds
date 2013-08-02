29a5360d7131090c5acad024c3b5ec56eda09a20
Andrew Bayer <andrew.bayer@gmail.com>
2013-08-02 09:55:51 -0700

    Fixing API casting for cloudsigma-zrh live test.

--- providers/cloudsigma-zrh/src/test/java/org/jclouds/cloudsigma/compute/CloudSigmaZurichComputeServiceLiveTest.java
+++ providers/cloudsigma-zrh/src/test/java/org/jclouds/cloudsigma/compute/CloudSigmaZurichComputeServiceLiveTest.java
@@ -61,7 +61,7 @@ public class CloudSigmaZurichComputeServiceLiveTest extends CloudSigmaComputeSer
          Set<? extends NodeMetadata> nodes = client.createNodesInGroup(group, 1, template);
          NodeMetadata node = get(nodes, 0);
 
-         CloudSigmaApi api = CloudSigmaApi.class.cast(client.getContext());
+         CloudSigmaApi api = client.getContext().unwrapApi(CloudSigmaApi.class);
 
          // Note: I wanted to use node.getHardware().getVolumes() but there is no
          // way to go from a Volume to a DriveInfo
